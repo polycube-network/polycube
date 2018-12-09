1fa9d1d2fd8afe6006eade814837582f4e27d601
Sebastiano Miano <mianosebastiano@gmail.com>
2018-12-09 17:15:55 +0100

    Allow pcn_log to be split in multiple lines
    This commit fixes a "bug" in the pcn_log function that did not allow
    to split the pcn_log body into multiple lines, for example by putting
    the text and the parameters in different lines.
    It uses an improved regex function to check the end of the
    pcn_log function and replace it with the correct version so that
    compilation errors can be avoided.
    
    Fixes: #1 (pcn_log cannot be split in multiple lines)
    Signed-off-by: Sebastiano Miano <mianosebastiano@gmail.com>

--- src/polycubed/src/datapath_log.h
+++ src/polycubed/src/datapath_log.h
@@ -19,6 +19,11 @@
 #include <functional>
 #include <thread>
 #include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <regex>
 
 #include <api/BPF.h>
 #include <spdlog/spdlog.h>
@@ -42,6 +47,9 @@ class DatapathLog {
   void stop();
 
   static void call_back_proxy(void *cb_cookie, void *data, int data_size);
+  static std::string replace_string(std::string& subject, const std::string& search, const std::string& replace);
+  static std::string dp_callback(const std::smatch& m);
+  static std::string dp_callback_pkt(const std::smatch& m);
 
   // replaces all the log calls to the code that does it
   std::string parse_log(const std::string &code);
@@ -60,3 +68,53 @@ class DatapathLog {
 
 }  // namespace polycubed
 }  // namespace polycube
+
+// Based on solution proposed in: https://stackoverflow.com/questions/22617209/regex-replace-with-callback-in-c11
+namespace std
+{
+
+template<class BidirIt, class Traits, class CharT, class UnaryFunction>
+std::basic_string<CharT> regex_replace_cb(BidirIt first, BidirIt last,
+    const std::basic_regex<CharT,Traits>& re, UnaryFunction f)
+{
+    std::basic_string<CharT> s;
+
+    typename std::match_results<BidirIt>::difference_type
+        positionOfLastMatch = 0;
+    auto endOfLastMatch = first;
+
+    auto callback = [&](const std::match_results<BidirIt>& match)
+    {
+        auto positionOfThisMatch = match.position(0);
+        auto diff = positionOfThisMatch - positionOfLastMatch;
+
+        auto startOfThisMatch = endOfLastMatch;
+        std::advance(startOfThisMatch, diff);
+
+        s.append(endOfLastMatch, startOfThisMatch);
+        s.append(f(match));
+
+        auto lengthOfMatch = match.length(0);
+
+        positionOfLastMatch = positionOfThisMatch + lengthOfMatch;
+
+        endOfLastMatch = startOfThisMatch;
+        std::advance(endOfLastMatch, lengthOfMatch);
+    };
+
+    std::regex_iterator<BidirIt> begin(first, last, re), end;
+    std::for_each(begin, end, callback);
+
+    s.append(endOfLastMatch, last);
+
+    return s;
+}
+
+template<class Traits, class CharT, class UnaryFunction>
+std::string regex_replace_cb(const std::string& s,
+    const std::basic_regex<CharT,Traits>& re, UnaryFunction f)
+{
+    return regex_replace_cb(s.cbegin(), s.cend(), re, f);
+}
+
+} // namespace std
