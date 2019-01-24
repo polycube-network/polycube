acbf961f30ed1620b31d0346ebb36f759e0c3fc9
SunSince90 <elis.lulja@gmail.com>
2019-01-24 21:17:36 +0100

    - created polycube_pod structure

--- src/components/k8s/pcn_k8s/controllers/pod_controller.go
+++ src/components/k8s/pcn_k8s/controllers/pod_controller.go
@@ -5,16 +5,17 @@ import (
 
 	//	TODO-ON-MERGE: change the path to polycube
 	events "github.com/SunSince90/polycube/src/components/k8s/pcn_k8s/types/events"
+	polycube_pod "github.com/SunSince90/polycube/src/components/k8s/pcn_k8s/types/polycubepod"
 )
 
 type PodController interface {
 	Run()
 	Stop()
-	Subscribe(events.EventType, func(*core_v1.Pod)) (func(), error)
+	Subscribe(events.EventType, func(*polycube_pod.Pod)) (func(), error)
 
-	GetPodsByName(string, string) (error, []core_v1.Pod, string)
-	AsyncGetPodsByName(chan<- []core_v1.Pod, string, string) (error, []core_v1.Pod, string)
+	GetPodsByName(string, string) ([]polycube_pod.Pod, error)
+	AsyncGetPodsByName(chan<- []polycube_pod.Pod, string, string) ([]polycube_pod.Pod, error)
 
-	GetPodsByLabels(map[string]string, string) (error, []core_v1.Pod, string)
-	AsyncGetPodsByLabels(chan<- []core_v1.Pod, map[string]string, string) (error, []core_v1.Pod, string)
+	GetPodsByLabels(map[string]string, string) ([]polycube_pod.Pod, error)
+	AsyncGetPodsByLabels(chan<- []polycube_pod.Pod, map[string]string, string) ([]core_v1.Pod, error)
 }
