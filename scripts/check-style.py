polycube-network/polycube

commit 67434e0686a61f54fa60e612a014e94d9f92d9d5

scripts/check-style: add reference to original script
Signed-off-by: Mauricio Vasquez B <mauriciovasquezbernal@gmail.com>

scripts/check-style.py
@@ -1,3 +1,5 @@
+# This script is based on https://github.com/Sarcasm/run-clang-format
+
 #!/usr/bin/env python
 """A wrapper script around clang-format, suitable for linting multiple files
 and to use for continuous integration.

commit 83de58d9e30f56766b8e94d34a42b8cbbb152325

scripts/checkstyle: move shebang to first line
A really stupid mistake by myself.

Fixes: 67434e0686a6 ("scripts/check-style: add reference to original script")

Signed-off-by: Mauricio Vasquez B <mauriciovasquezbernal@gmail.com>

scripts/check-style.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python
+
 # This script is based on https://github.com/Sarcasm/run-clang-format
 
-#!/usr/bin/env python
 """A wrapper script around clang-format, suitable for linting multiple files
 and to use for continuous integration.
 