polycube-network/polycube

commit 9d00993224ff358760418ac26487ab71fd05fcbb

create base cube
Create a common base class for standard and transparent cubes

Signed-off-by: Mauricio Vasquez B <mauriciovasquezbernal@gmail.com>

src/libs/polycube/include/polycube/services/base_cube.h
@@ -1,0 +1,136 @@
+/*
+ * Copyright 2018 The Polycube Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <map>
+#include <string>
+
+#include <spdlog/sinks/rotating_file_sink.h>
+#include <spdlog/sinks/stdout_sinks.h>
+#include <spdlog/spdlog.h>
+
+#include "polycube/common.h"
+
+#include "polycube/services/cube_factory.h"
+#include "polycube/services/cube_iface.h"
+#include "polycube/services/management_interface.h"
+#include "polycube/services/table.h"
+#include "polycube/services/utils.h"
+
+namespace polycube {
+namespace service {
+
+extern CubeFactory *factory_;
+extern std::string logfile_;
+
+class BaseCube {
+ public:
+  BaseCube(const std::string &name,
+           const std::vector<std::string> &ingress_code,
+           const std::vector<std::string> &egress_code, const CubeType type,
+           LogLevel level = LogLevel::OFF);
+  virtual ~BaseCube();
+
+  // TODO: implement move constructor, forbid copy and asignment
+
+  void reload(const std::string &code, int index = 0,
+              ProgramType type = ProgramType::INGRESS);
+  int add_program(const std::string &code, int index = -1,
+                  ProgramType type = ProgramType::INGRESS);
+  void del_program(int index, ProgramType type = ProgramType::INGRESS);
+
+  // Accessors for tables
+  RawTable get_raw_table(const std::string &table_name, int index = 0,
+                         ProgramType type = ProgramType::INGRESS);
+  template <class ValueType>
+  ArrayTable<ValueType> get_array_table(
+      const std::string &table_name, int index = 0,
+      ProgramType type = ProgramType::INGRESS);
+  template <class ValueType>
+  PercpuArrayTable<ValueType> get_percpuarray_table(
+      const std::string &table_name, int index = 0,
+      ProgramType type = ProgramType::INGRESS);
+  template <class KeyType, class ValueType>
+  HashTable<KeyType, ValueType> get_hash_table(
+      const std::string &table_name, int index = 0,
+      ProgramType type = ProgramType::INGRESS);
+  template <class KeyType, class ValueType>
+  PercpuHashTable<KeyType, ValueType> get_percpuhash_table(
+      const std::string &table_name, int index = 0,
+      ProgramType type = ProgramType::INGRESS);
+
+  // virtual void packet_in(PortType &port, PacketInMetadata &md,
+  //                       const std::vector<uint8_t> &packet) = 0;
+
+  virtual void datapath_log_msg(const LogMsg *msg);
+
+  void set_log_level(LogLevel level);
+  LogLevel get_log_level() const;
+
+  const Guid &get_uuid() const;
+  const std::string get_name() const;
+  CubeType get_type() const;
+
+  // protected: (later on)
+  std::shared_ptr<spdlog::logger> logger();
+
+  void dismount();
+
+ protected:
+  int get_table_fd(const std::string &table_name, int index, ProgramType type);
+
+  std::shared_ptr<BaseCubeIface> cube_;  // pointer to the cube in polycubed
+  // packet_in_cb handle_packet_in;
+  log_msg_cb handle_log_msg;
+  CubeType type_;
+  std::shared_ptr<spdlog::logger> logger_;
+  std::atomic<bool> dismounted_;
+
+  std::mutex cube_mutex;
+};
+
+template <class ValueType>
+ArrayTable<ValueType> BaseCube::get_array_table(const std::string &table_name,
+                                                int index, ProgramType type) {
+  int fd = get_table_fd(table_name, index, type);
+  return ArrayTable<ValueType>(&fd);
+};
+
+template <class ValueType>
+PercpuArrayTable<ValueType> BaseCube::get_percpuarray_table(
+    const std::string &table_name, int index, ProgramType type) {
+  int fd = get_table_fd(table_name, index, type);
+  return PercpuArrayTable<ValueType>(&fd);
+  ;
+}
+
+template <class KeyType, class ValueType>
+HashTable<KeyType, ValueType> BaseCube::get_hash_table(
+    const std::string &table_name, int index, ProgramType type) {
+  int fd = get_table_fd(table_name, index, type);
+  return HashTable<KeyType, ValueType>(&fd);
+}
+
+template <class KeyType, class ValueType>
+PercpuHashTable<KeyType, ValueType> BaseCube::get_percpuhash_table(
+    const std::string &table_name, int index, ProgramType type) {
+  int fd = get_table_fd(table_name, index, type);
+  return PercpuHashTable<KeyType, ValueType>(&fd);
+}
+
+}  // namespace service
+}  // namespace polycube

src/libs/polycube/src/base_cube.cpp
@@ -1,0 +1,162 @@
+/*
+ * Copyright 2018 The Polycube Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "polycube/services/base_cube.h"
+
+namespace polycube {
+namespace service {
+
+BaseCube::BaseCube(const std::string &name,
+                   const std::vector<std::string> &ingress_code,
+                   const std::vector<std::string> &egress_code,
+                   const CubeType type, LogLevel level)
+    : type_(type),
+      dismounted_(false),
+      logger_(std::make_shared<spdlog::logger>(
+          name, (spdlog::sinks_init_list){
+                    std::make_shared<spdlog::sinks::rotating_file_sink_mt>(
+                        logfile_, 1048576 * 5, 3),
+                    std::make_shared<spdlog::sinks::stdout_sink_mt>()})) {
+  logger()->set_level(logLevelToSPDLog(level));
+  // TODO: move to function
+  /*
+  handle_packet_in = [&](const PacketIn *md,
+                         const std::vector<uint8_t> &packet) -> void {
+    // This lock guarantees:
+    // - port is not deleted while processing it
+    // - service implementation is not deleted wile processing it
+    std::lock_guard<std::mutex> guard(cube_mutex);
+    if (dismounted_)
+      return;
+
+    auto &p = *ports_by_id_.at(md->port_id);
+    PacketInMetadata md_;
+    md_.reason = md->reason;
+    md_.metadata[0] = md->metadata[0];
+    md_.metadata[1] = md->metadata[1];
+    md_.metadata[2] = md->metadata[2];
+    packet_in(p, md_, packet);
+  };
+  */
+
+  handle_log_msg = [&](const LogMsg *msg) -> void { datapath_log_msg(msg); };
+
+  // TODO: where to create cube?, here or in derived classes?
+  // cube_ = factory_->create_cube(name, ingress_code, egress_code,
+  //                              handle_log_msg, type, handle_packet_in,
+  //                              level);
+}
+
+BaseCube::~BaseCube() {
+  // just in case
+  dismount();
+
+  // factory_->destroy_cube(get_name());
+}
+
+int BaseCube::get_table_fd(const std::string &table_name, int index,
+                           ProgramType type) {
+  return cube_->get_table_fd(table_name, index, type);
+}
+
+void BaseCube::reload(const std::string &code, int index, ProgramType type) {
+  cube_->reload(code, index, type);
+}
+
+int BaseCube::add_program(const std::string &code, int index,
+                          ProgramType type) {
+  return cube_->add_program(code, index, type);
+}
+
+void BaseCube::del_program(int index, ProgramType type) {
+  cube_->del_program(index, type);
+}
+
+RawTable BaseCube::get_raw_table(const std::string &table_name, int index,
+                                 ProgramType type) {
+  int fd = get_table_fd(table_name, index, type);
+  RawTable t(&fd);
+  return std::move(t);
+}
+
+void BaseCube::datapath_log_msg(const LogMsg *msg) {
+  spdlog::level::level_enum level_ =
+      logLevelToSPDLog((polycube::LogLevel)msg->level);
+  std::string print;
+
+  switch (msg->type) {
+  case 0:
+    print = utils::format_debug_string(msg->msg, msg->args);
+    logger()->log(level_, print.c_str());
+    break;
+
+  case 1:
+#ifdef HAVE_POLYCUBE_TOOLS
+    logger()->log(level_, "packet received for debug:");
+    utils::print_packet((const uint8_t *)msg->msg, msg->len);
+#else
+    logger()->warn(
+        "Received packet for debugging. polycube-tools is not available");
+#endif
+    break;
+
+  default:
+    logger()->warn("Received bad message type in datapath_log_msg");
+    return;
+  }
+}
+
+void BaseCube::set_log_level(LogLevel level) {
+  logger()->set_level(logLevelToSPDLog(level));
+  return cube_->set_log_level(level);
+}
+
+LogLevel BaseCube::get_log_level() const {
+  return cube_->get_log_level();
+}
+
+const Guid &BaseCube::get_uuid() const {
+  return cube_->uuid();
+}
+
+const std::string BaseCube::get_name() const {
+  return cube_->get_name();
+}
+
+CubeType BaseCube::get_type() const {
+  return type_;
+}
+
+std::shared_ptr<spdlog::logger> BaseCube::logger() {
+  return logger_;
+}
+
+void BaseCube::dismount() {
+  std::lock_guard<std::mutex> guard(cube_mutex);
+
+  if (dismounted_)
+    return;
+
+  dismounted_ = true;
+  // invalidate handlers
+  // handle_packet_in = nullptr;
+  handle_log_msg = nullptr;
+
+  // TODO: remove from controller and datapathlog?
+}
+
+}  // namespace service
+}  // namespace polycube

src/polycubed/src/base_cube.cpp
@@ -1,0 +1,465 @@
+/*
+ * Copyright 2018 The Polycube Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "base_cube.h"
+
+namespace polycube {
+namespace polycubed {
+
+std::vector<std::string> BaseCube::cflags = {
+    std::string("-D_POLYCUBE_MAX_NODES=") +
+        std::to_string(Node::_POLYCUBE_MAX_NODES),
+    std::string("-D_POLYCUBE_MAX_BPF_PROGRAMS=") +
+        std::to_string(_POLYCUBE_MAX_BPF_PROGRAMS),
+    std::string("-D_POLYCUBE_MAX_PORTS=") + std::to_string(_POLYCUBE_MAX_PORTS),
+};
+
+BaseCube::BaseCube(const std::string &name, const std::string &service_name,
+                   const std::string &master_code,
+                   PatchPanel &patch_panel_ingress,
+                   PatchPanel &patch_panel_egress, LogLevel level,
+                   CubeType type)
+    : name_(name),
+      service_name_(service_name),
+      logger(spdlog::get("polycubed")),
+      uuid_(GuidGenerator().newGuid()),
+      patch_panel_ingress_(patch_panel_ingress),
+      patch_panel_egress_(patch_panel_egress),
+      ingress_fd_(0),
+      egress_fd_(0),
+      ingress_index_(0),
+      egress_index_(0),
+      level_(level),
+      type_(type),
+      id_(id_generator_.acquire()) {
+  std::lock_guard<std::mutex> guard(bcc_mutex);
+
+  // create master program that contains some maps definitions
+  master_program_ =
+      std::unique_ptr<ebpf::BPF>(new ebpf::BPF(0, nullptr, false, name));
+  master_program_->init(BASECUBE_MASTER_CODE + master_code, cflags);
+
+  // get references to those maps
+  auto ingress_ = master_program_->get_prog_table("ingress_programs");
+  ingress_programs_table_ =
+      std::unique_ptr<ebpf::BPFProgTable>(new ebpf::BPFProgTable(ingress_));
+
+  auto egress_ = master_program_->get_prog_table("egress_programs");
+  egress_programs_table_ =
+      std::unique_ptr<ebpf::BPFProgTable>(new ebpf::BPFProgTable(egress_));
+}
+
+void BaseCube::init(const std::vector<std::string> &ingress_code,
+                    const std::vector<std::string> &egress_code) {
+  for (int i = 0; i < ingress_code.size(); i++) {
+    add_program(ingress_code[i], i, ProgramType::INGRESS);
+  }
+
+  for (int i = 0; i < egress_code.size(); i++) {
+    add_program(egress_code[i], i, ProgramType::EGRESS);
+  }
+}
+
+void BaseCube::uninit() {
+  if (ingress_index_)
+    patch_panel_ingress_.remove(ingress_index_);
+
+  if (egress_index_)
+    patch_panel_egress_.remove(egress_index_);
+
+  for (int i = 0; i < ingress_programs_.size(); i++) {
+    if (ingress_programs_[i]) {
+      del_program(i, ProgramType::INGRESS);
+    }
+  }
+
+  for (int i = 0; i < egress_programs_.size(); i++) {
+    if (egress_programs_[i]) {
+      del_program(i, ProgramType::EGRESS);
+    }
+  }
+}
+
+BaseCube::~BaseCube() {
+  id_generator_.release(id_);
+}
+
+uint32_t BaseCube::get_id() const {
+  return id_;
+}
+
+uint16_t BaseCube::get_index(ProgramType type) const {
+  switch (type) {
+  case ProgramType::INGRESS:
+    return ingress_index_;
+  case ProgramType::EGRESS:
+    return egress_index_;
+  }
+  return 0;
+}
+
+CubeType BaseCube::get_type() const {
+  return type_;
+}
+
+const std::string BaseCube::get_name() const {
+  return name_;
+}
+
+const std::string BaseCube::get_service_name() const {
+  return service_name_;
+}
+
+const Guid &BaseCube::uuid() const {
+  return uuid_;
+}
+
+int BaseCube::get_table_fd(const std::string &table_name, int index,
+                           ProgramType type) {
+  // TODO: Improve bcc api to do it
+  switch (type) {
+  case ProgramType::INGRESS:
+    return ingress_programs_[index]->get_table(table_name).fd();
+  case ProgramType::EGRESS:
+    return egress_programs_[index]->get_table(table_name).fd();
+  }
+}
+
+void BaseCube::reload(const std::string &code, int index, ProgramType type) {
+  std::lock_guard<std::mutex> cube_guard(cube_mutex_);
+  return do_reload(code, index, type);
+}
+
+void BaseCube::do_reload(const std::string &code, int index, ProgramType type) {
+  std::array<std::unique_ptr<ebpf::BPF>, _POLYCUBE_MAX_BPF_PROGRAMS> *programs;
+  std::array<std::string, _POLYCUBE_MAX_BPF_PROGRAMS> *code_;
+  int fd;
+
+  switch (type) {
+  case ProgramType::INGRESS:
+    programs = &ingress_programs_;
+    code_ = &ingress_code_;
+    break;
+  case ProgramType::EGRESS:
+    programs = &egress_programs_;
+    code_ = &egress_code_;
+    break;
+  default:
+    throw std::runtime_error("Bad program type");
+  }
+
+  if (index >= programs->size()) {
+    throw std::runtime_error("Invalid ebpf program index");
+  }
+
+  if (programs->at(index) == nullptr) {
+    throw std::runtime_error("ebpf does not exist");
+  }
+
+  // create new ebpf program, telling to steal the maps of this program
+  std::unique_lock<std::mutex> bcc_guard(bcc_mutex);
+  std::unique_ptr<ebpf::BPF> new_bpf_program = std::unique_ptr<ebpf::BPF>(
+      new ebpf::BPF(0, nullptr, false, name_, &*programs->at(index)));
+
+  bcc_guard.unlock();
+  compile(*new_bpf_program, code, index, type);
+  fd = load(*new_bpf_program, type);
+
+  switch (type) {
+  case ProgramType::INGRESS:
+    ingress_programs_table_->update_value(index, fd);
+    // update patch panel if program is master
+    if (index == 0) {
+      ingress_fd_ = fd;
+      patch_panel_ingress_.update(ingress_index_, ingress_fd_);
+    }
+    break;
+  case ProgramType::EGRESS:
+    egress_programs_table_->update_value(index, fd);
+    // update patch panel if program is master
+    if (index == 0) {
+      egress_fd_ = fd;
+      patch_panel_egress_.update(egress_index_, egress_fd_);
+    }
+    break;
+  }
+
+  unload(*programs->at(index), type);
+  bcc_guard.lock();
+  (*programs)[index] = std::move(new_bpf_program);
+  // update last used code
+  (*code_)[index] = code;
+}
+
+void BaseCube::reload_all() {
+  std::lock_guard<std::mutex> cube_guard(cube_mutex_);
+  for (int i = 0; i < ingress_programs_.size(); i++) {
+    if (ingress_programs_[i]) {
+      do_reload(ingress_code_[i], i, ProgramType::INGRESS);
+    }
+  }
+
+  for (int i = 0; i < egress_programs_.size(); i++) {
+    if (egress_programs_[i]) {
+      do_reload(egress_code_[i], i, ProgramType::EGRESS);
+    }
+  }
+}
+
+int BaseCube::add_program(const std::string &code, int index,
+                          ProgramType type) {
+  std::lock_guard<std::mutex> cube_guard(cube_mutex_);
+
+  std::array<std::unique_ptr<ebpf::BPF>, _POLYCUBE_MAX_BPF_PROGRAMS> *programs;
+  std::array<std::string, _POLYCUBE_MAX_BPF_PROGRAMS> *code_;
+
+  switch (type) {
+  case ProgramType::INGRESS:
+    programs = &ingress_programs_;
+    code_ = &ingress_code_;
+    break;
+  case ProgramType::EGRESS:
+    programs = &egress_programs_;
+    code_ = &egress_code_;
+    break;
+  default:
+    throw std::runtime_error("Bad program type");
+  }
+
+  // check index boundaries
+  if (index < 0 || index >= programs->size()) {
+    throw std::runtime_error("Invalid ebpf program index");
+  }
+
+  if (index != -1) {
+    if (programs->at(index) != nullptr) {
+      throw std::runtime_error("Program is not empty");
+    }
+  } else {
+    int i;
+    for (i = 0; i < programs->size(); i++) {
+      if (programs->at(i) == nullptr) {
+        index = i;
+        break;
+      }
+    }
+
+    if (i == programs->size()) {
+      throw std::runtime_error("Maximum number of programs reached");
+    }
+  }
+
+  std::unique_lock<std::mutex> bcc_guard(bcc_mutex);
+  // load and add this program to the list
+  int fd_;
+  (*programs)[index] =
+      std::unique_ptr<ebpf::BPF>(new ebpf::BPF(0, nullptr, false, name_));
+
+  bcc_guard.unlock();
+  compile(*programs->at(index), code, index, type);
+  fd_ = load(*programs->at(index), type);
+  bcc_guard.lock();
+
+  // TODO: this switch could also be removed creating variables in the switch
+  // above
+  switch (type) {
+  case ProgramType::INGRESS:
+    ingress_programs_table_->update_value(index, fd_);
+    if (index == 0) {
+      ingress_fd_ = fd_;
+      if (ingress_index_) {
+        // already registed in patch panel, just update
+        patch_panel_ingress_.update(ingress_index_, ingress_fd_);
+      } else {
+        // register in patch panel
+        ingress_index_ = patch_panel_ingress_.add(ingress_fd_);
+      }
+    }
+    break;
+  case ProgramType::EGRESS:
+    egress_programs_table_->update_value(index, fd_);
+    if (index == 0) {
+      egress_fd_ = fd_;
+      if (egress_index_) {
+        // already registed in patch panel, just update
+        patch_panel_egress_.update(egress_index_, egress_fd_);
+      } else {
+        // register in patch panel
+        egress_index_ = patch_panel_egress_.add(egress_fd_);
+      }
+      break;
+    }
+  }
+
+  (*code_)[index] = code;
+
+  return index;
+}
+
+void BaseCube::del_program(int index, ProgramType type) {
+  std::lock_guard<std::mutex> cube_guard(cube_mutex_);
+
+  std::array<std::unique_ptr<ebpf::BPF>, _POLYCUBE_MAX_BPF_PROGRAMS> *programs;
+  std::array<std::string, _POLYCUBE_MAX_BPF_PROGRAMS> *code_;
+  ebpf::BPFProgTable *programs_table;
+
+  switch (type) {
+  case ProgramType::INGRESS:
+    programs = &ingress_programs_;
+    code_ = &ingress_code_;
+    programs_table = ingress_programs_table_.get();  // TODO: is this valid?
+    break;
+  case ProgramType::EGRESS:
+    programs = &egress_programs_;
+    code_ = &egress_code_;
+    programs_table = egress_programs_table_.get();  // TODO: is this valid?
+    break;
+  default:
+    throw std::runtime_error("Bad program type");
+  }
+
+  if (index < 0 || index >= programs->size()) {
+    logger->error("Invalid ebpf program index");
+    throw std::runtime_error("Invalid ebpf program index");
+  }
+
+  if (programs->at(index) == nullptr) {
+    throw std::runtime_error("ebpf program does not exist");
+  }
+
+  programs_table->remove_value(index);
+  unload(*programs->at(index), type);
+  std::unique_lock<std::mutex> bcc_guard(bcc_mutex);
+  programs->at(index).reset();
+  code_->at(index).clear();
+}
+
+std::string BaseCube::get_wrapper_code() {
+  return BASECUBE_WRAPPER;
+}
+
+void BaseCube::set_log_level(LogLevel level) {
+  if (level_ == level)
+    return;
+
+  level_ = level;
+  reload_all();
+}
+
+LogLevel BaseCube::get_log_level() const {
+  return level_;
+}
+
+json BaseCube::to_json() const {
+  json j = {
+      {"name", name_}, {"uuid", uuid_.str()}, {"service", service_name_},
+      //{"type", type_},
+  };
+
+  return j;
+}
+
+IDGenerator BaseCube::id_generator_(Node::_POLYCUBE_MAX_NODES - 2);
+
+const std::string BaseCube::BASECUBE_MASTER_CODE = R"(
+// tables to save file descriptor of ingress and egress programs
+BPF_TABLE_SHARED("prog", int, int, ingress_programs, _POLYCUBE_MAX_BPF_PROGRAMS);
+BPF_TABLE_SHARED("prog", int, int, egress_programs, _POLYCUBE_MAX_BPF_PROGRAMS);
+)";
+
+const std::string BaseCube::BASECUBE_WRAPPER = R"(
+#define KBUILD_MODNAME "MOD_NAME"
+#include <bcc/helpers.h>
+#include <bcc/proto.h>
+#include <uapi/linux/pkt_cls.h>
+
+#include <uapi/linux/bpf.h>
+
+#define CONTROLLER_MODULE_INDEX (_POLYCUBE_MAX_NODES - 1)
+
+// maps definitions, same as in master program but "extern"
+BPF_TABLE("extern", int, int, ingress_programs, _POLYCUBE_MAX_BPF_PROGRAMS);
+BPF_TABLE("extern", int, int, egress_programs, _POLYCUBE_MAX_BPF_PROGRAMS);
+
+enum {
+  RX_OK,
+  RX_REDIRECT,
+  RX_DROP,
+  RX_RECIRCULATE,
+  RX_CONTROLLER,
+  RX_ERROR,
+};
+
+struct pkt_metadata {
+  u16 cube_id;      //__attribute__((deprecated)) // use CUBE_ID instead
+  u16 in_port;      // The interface on which a packet was received.
+  u32 packet_len;   //__attribute__((deprecated)) // Use ctx->len
+
+  // used to send data to controller
+  u16 reason;
+  u32 md[3];
+} __attribute__((packed));
+
+// cube must implement this function to attach to the networking stack
+static __always_inline
+int handle_rx(struct CTXTYPE *ctx, struct pkt_metadata *md);
+
+static __always_inline
+int pcn_pkt_redirect(struct CTXTYPE *skb,
+                     struct pkt_metadata *md, u32 port);
+
+static __always_inline
+int pcn_pkt_drop(struct CTXTYPE *skb, struct pkt_metadata *md);
+
+static __always_inline
+int pcn_pkt_controller(struct CTXTYPE *skb, struct pkt_metadata *md,
+                       u16 reason);
+
+static __always_inline
+int pcn_pkt_controller_with_metadata(struct CTXTYPE *skb,
+                                     struct pkt_metadata *md,
+                                     u16 reason,
+                                     u32 metadata[3]);
+
+static __always_inline
+void call_ingress_program(struct CTXTYPE *skb, int index) {
+  ingress_programs.call(skb, index);
+}
+
+static __always_inline
+void call_egress_program(struct CTXTYPE *skb, int index) {
+  egress_programs.call(skb, index);
+}
+
+/* checksum related */
+
+// those functions have different implementations for XDP and TC
+static __always_inline
+int pcn_l3_csum_replace(struct CTXTYPE *ctx, u32 csum_offset,
+                        u32 old_value, u32 new_value, u32 flags);
+
+static __always_inline
+int pcn_l4_csum_replace(struct CTXTYPE *ctx, u32 csum_offset,
+                        u32 old_value, u32 new_value, u32 flags);
+
+static __always_inline
+__wsum pcn_csum_diff(__be32 *from, u32 from_size, __be32 *to,
+                     u32 to_size, __wsum seed);
+
+)";
+
+}  // namespace polycubed
+}  // namespace polycube

commit 986261b939b258d802c74844abba74d5d626b6be

libpolycube: remove dead code in cube
Remove some dead code related to packet in functionality in the base cube
class

Fixes: 9d00993224ff ("create base cube")

Signed-off-by: Mauricio Vasquez B <mauriciovasquezbernal@gmail.com>

src/libs/polycube/include/polycube/services/base_cube.h
@@ -73,9 +73,6 @@ class BaseCube {
       const std::string &table_name, int index = 0,
       ProgramType type = ProgramType::INGRESS);
 
-  // virtual void packet_in(PortType &port, PacketInMetadata &md,
-  //                       const std::vector<uint8_t> &packet) = 0;
-
   virtual void datapath_log_msg(const LogMsg *msg);
 
   void set_log_level(LogLevel level);
@@ -94,7 +91,6 @@ class BaseCube {
   int get_table_fd(const std::string &table_name, int index, ProgramType type);
 
   std::shared_ptr<BaseCubeIface> cube_;  // pointer to the cube in polycubed
-  // packet_in_cb handle_packet_in;
   log_msg_cb handle_log_msg;
   CubeType type_;
   std::shared_ptr<spdlog::logger> logger_;

src/libs/polycube/src/base_cube.cpp
@@ -31,40 +31,12 @@ BaseCube::BaseCube(const std::string &name,
                         logfile_, 1048576 * 5, 3),
                     std::make_shared<spdlog::sinks::stdout_sink_mt>()})) {
   logger()->set_level(logLevelToSPDLog(level));
-  // TODO: move to function
-  /*
-  handle_packet_in = [&](const PacketIn *md,
-                         const std::vector<uint8_t> &packet) -> void {
-    // This lock guarantees:
-    // - port is not deleted while processing it
-    // - service implementation is not deleted wile processing it
-    std::lock_guard<std::mutex> guard(cube_mutex);
-    if (dismounted_)
-      return;
-
-    auto &p = *ports_by_id_.at(md->port_id);
-    PacketInMetadata md_;
-    md_.reason = md->reason;
-    md_.metadata[0] = md->metadata[0];
-    md_.metadata[1] = md->metadata[1];
-    md_.metadata[2] = md->metadata[2];
-    packet_in(p, md_, packet);
-  };
-  */
-
   handle_log_msg = [&](const LogMsg *msg) -> void { datapath_log_msg(msg); };
-
-  // TODO: where to create cube?, here or in derived classes?
-  // cube_ = factory_->create_cube(name, ingress_code, egress_code,
-  //                              handle_log_msg, type, handle_packet_in,
-  //                              level);
 }
 
 BaseCube::~BaseCube() {
   // just in case
   dismount();
-
-  // factory_->destroy_cube(get_name());
 }
 
 int BaseCube::get_table_fd(const std::string &table_name, int index,
@@ -152,7 +124,6 @@ void BaseCube::dismount() {
 
   dismounted_ = true;
   // invalidate handlers
-  // handle_packet_in = nullptr;
   handle_log_msg = nullptr;
 
   // TODO: remove from controller and datapathlog?

src/libs/polycube/src/transparent_cube.cpp
@@ -54,7 +54,7 @@ TransparentCube::~TransparentCube() {
   // just in case
   dismount();
 
-  // handle_packet_in = nullptr;
+  handle_packet_in = nullptr;
 
   factory_->destroy_cube(get_name());
 }