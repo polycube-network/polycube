0d5dcd5a43224ec2e620467208b921a35fe493a0
Elis <34626570+SunSince90@users.noreply.github.com>
2019-03-05 14:34:17 +0100

    Update object_query.go

--- src/components/k8s/pcn_k8s/types/object_query.go
+++ src/components/k8s/pcn_k8s/types/object_query.go
@@ -1,7 +1,7 @@
 package types
 
-// QueryObject is a struct that specifies how the object should be found
-type QueryObject struct {
+// ObjectQuery is a struct that specifies how the object should be found
+type ObjectQuery struct {
 	By     string
 	Name   string
 	Labels map[string]string
