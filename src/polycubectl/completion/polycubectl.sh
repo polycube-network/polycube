polycube-network/polycube

commit 95aa7415d741166fd3b30af56a2c685ae3888567

implement bash autocompletion
bash completion speeds up the writing of long commands, it also helps the
user with the different available options for a given command.

Signed-off-by: Mauricio Vasquez B <mauriciovasquezbernal@gmail.com>

src/polycubectl/completion/polycubectl.sh
@@ -1,0 +1,158 @@
+#/usr/bin/env bash
+
+parse_yaml() {
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
+
+# Use bash-completion, if available
+[[ $PS1 && -f /usr/share/bash-completion/bash_completion ]] && \
+    . /usr/share/bash-completion/bash_completion
+
+# TODO: Get this from configuration file and env variable
+BASE_URL_="http://localhost:9000/polycube/v1/"
+
+_polycubectl_completions() {
+  if [ -a "$HOME/.config/polycube/polycubectl_config.yaml" ]
+  then
+    eval $(parse_yaml "$HOME/.config/polycube/polycubectl_config.yaml" "config_")
+  fi
+
+  BASE_URL=${POLYCUBECTL_URL:-$config_url}
+  BASE_URL=${BASE_URL:-BASE_URL_}
+  local cur prev
+  _get_comp_words_by_ref -n ":,=" cur prev
+  local words
+  _get_comp_words_by_ref -n ":,=" -w words
+
+  OLDIFS=$IFS
+  COMP=""
+
+  URL0=""
+  HELP_TYPE="NONE"
+  i=0
+  for X in ${words[@]}
+  do
+    let i++
+
+    # do not consider the current word the user is typing
+    #if [ $i == $((COMP_CWORD+1)) ]
+    if [ "$X" == "$cur" ]
+    then
+      #break
+      continue
+    fi
+
+    if [[ $X == *"="* ]]
+    then
+      continue
+    fi
+
+    if [[ $X == *":"* ]]
+    then
+      continue
+    fi
+
+    if [[ "polycubectl" == $X ]]
+    then
+      continue
+    fi
+
+    if [[ "add" == $X || "del" == $X || "show" == $X || "set" == $X ]]
+    then
+      HELP_TYPE=`echo $X | awk '{print toupper($0)}'`
+      continue
+    fi
+
+    #echo $X
+    URL0+=$X
+    URL0+='/'
+  done
+
+  #if [ "${cur: -1}" == "=" ]
+  if [[ $cur == *"="* ]]
+  then
+    URL0+="${cur%%=*}"
+    URL0+='/'
+  fi
+
+  URL0="${URL0%?}"  # remove last '/'
+  URL=$BASE_URL$URL0"?help="$HELP_TYPE"&completion"
+
+  #echo "URL is: " $URL
+  #return
+
+  JSON=`curl -f -L -s -X OPTIONS $URL`
+  if [ $? -ne 0 ]
+  then
+    return
+  fi
+
+  #echo $JSON
+
+  VEC="`echo $JSON | jq -cr '.? // [] | @tsv'`"
+
+  if [ ${#VEC[@]} == 1 ]
+  then
+    VEC0=${VEC[0]}
+    if [[ ${VEC0:0:1} == "<" ]]
+    then
+      # if the command is ADD then print the keyname that is expected, but
+      # without creating a completion
+      printf "\n${VEC0}" >/dev/tty
+      expandedPrompt=$(PS1="$PS1" debian_chroot="$debian_chroot" "$BASH" --norc -i </dev/null 2>&1 | sed -n '${s/^\(.*\)exit$/\1/p;}')
+      printf '\n%s%s' "$expandedPrompt" "$COMP_LINE" >/dev/tty
+      return 0;
+    fi
+  fi
+
+  IFS=$'\t'
+  for CMD in ${VEC[@]}
+  do
+    # add space for commands that doesn't end in "="
+    if [ ${CMD: -1} != "=" ]
+    then
+      CMD+=" "
+    fi
+
+    if [[ $cur == *":"* ]]
+    then
+      CMD="${CMD#*:}"
+    fi
+
+    COMP+="$CMD"$IFS
+  done
+
+  IFS=$'\t\n'
+
+  local cur0=$cur
+
+  # if "=" or ":" are present in the current word, get the substring after it,
+  # otherwise the completion is going to give wrong results
+  if [[ $cur == *"="* ]]
+  then
+    cur0="${cur#*=}"
+  fi
+
+  if [[ $cur == *":"* ]]
+  then
+    cur0="${cur#*:}"
+  fi
+
+  COMPREPLY=( $(compgen -W "$COMP" -- $cur0) )
+
+  IFS=$OLDIFS
+}
+
+complete -o nospace -F _polycubectl_completions polycubectl

scripts/install.sh
@@ -82,6 +82,7 @@ PACKAGES+=" autoconf libtool m4 automake"
 PACKAGES+=" libssl-dev" # needed for certificate based security
 PACKAGES+=" sudo" # needed for pcn-iptables, when building docker image
 PACKAGES+=" kmod" # needed for pcn-iptables, when using lsmod to unload conntrack if not needed
+PACKAGES+=" jq" # needed for polycubectl bash autocompletion
 
 if [ "$MODE" == "pcn-k8s" ]; then
   PACKAGES+=" curl" # needed for pcn-k8s to download a binary

src/polycubectl/CMakeLists.txt
@@ -2,3 +2,34 @@ configure_file(version.go.in ${CMAKE_CURRENT_SOURCE_DIR}//version.go)
 install(CODE
 "execute_process( COMMAND /bin/sh -c \"GOPATH=$ENV{GOPATH} USER=$ENV{USER} ${CMAKE_SOURCE_DIR}/src/polycubectl/install.sh\"
 WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/src/polycubectl/ )")
+
+
+# this is the right approach but it is not working on ubuntu, for some reason
+# the cmake package is not installed in ubuntu
+
+#find_package(bash-completion)
+#if(BASH_COMPLETION_FOUND)
+#  message(STATUS
+#    "Using bash completion dir ${BASH_COMPLETION_COMPLETIONSDIR}")
+#else()
+#  set (BASH_COMPLETION_COMPLETIONSDIR "/etc/bash_completion.d")
+#  message (STATUS
+#    "Using fallback bash completion dir ${BASH_COMPLETION_COMPLETIONSDIR}")
+#endif()
+#
+#install(completion/polycubectl.sh polycubectl DESTINATION
+#  ${BASH_COMPLETION_COMPLETIONSDIR})
+
+# get the path were to install the completion scripts
+# more info (https://github.com/scop/bash-completion)
+execute_process (
+  COMMAND pkg-config --variable=completionsdir bash-completion
+  OUTPUT_VARIABLE COMPLETION_PATH
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+install(
+  FILES completion/polycubectl.sh
+  DESTINATION ${COMPLETION_PATH}
+  RENAME polycubectl
+)

src/polycubed/src/cube.cpp
@@ -153,6 +153,10 @@ std::shared_ptr<PortIface> Cube::get_port(const std::string &name) {
   return ports_by_name_.at(name);
 }
 
+std::map<std::string, std::shared_ptr<PortIface>> &Cube::get_ports() {
+  return ports_by_name_;
+}
+
 void Cube::update_forwarding_table(int index, int value) {
   std::lock_guard<std::mutex> cube_guard(cube_mutex_);
   if (forward_chain_)  // is the forward chain still active?

src/polycubed/src/cube.h
@@ -59,6 +59,7 @@ class Cube : public BaseCube, public CubeIface {
                                       const nlohmann::json &conf);
   void remove_port(const std::string &name);
   std::shared_ptr<PortIface> get_port(const std::string &name);
+  std::map<std::string, std::shared_ptr<PortIface>> &get_ports();
 
   void update_forwarding_table(int index, int value);
 

src/polycubed/src/polycubed_core.cpp
@@ -478,6 +478,23 @@ std::string PolycubedCore::set_cube_parameter(const std::string &cube,
       "PolycubedCore::set_cube_parameter is not implemented");
 }
 
+std::vector<std::string> PolycubedCore::get_all_ports() {
+  std::vector<std::string> ports;
+  auto cubes = ServiceController::get_all_cubes();
+  for (auto &cube_ : cubes) {
+    auto cube = std::dynamic_pointer_cast<Cube>(cube_);
+    if (!cube) {
+      continue;
+    }
+
+    for (auto &it : cube->get_ports()) {
+      ports.push_back(cube->get_name() + ":" + it.first);
+    }
+  }
+
+  return std::move(ports);
+}
+
 void PolycubedCore::set_rest_server(RestServer *rest_server) {
   rest_server_ = rest_server;
 }

src/polycubed/src/polycubed_core.h
@@ -79,6 +79,8 @@ class PolycubedCore {
                                  const std::string &parameter,
                                  const std::string &value);
 
+  std::vector<std::string> get_all_ports();
+
   void set_rest_server(RestServer *rest_server);
   RestServer *get_rest_server();
   BaseModel *base_model();

src/polycubed/src/rest_server.cpp
@@ -199,6 +199,9 @@ void RestServer::setup_routes() {
   router_->post(base + std::string("/disconnect"),
                 bind(&RestServer::disconnect, this));
 
+  router_->options(base + std::string("/connect"),
+                   bind(&RestServer::connect_help, this));
+
   // attach & detach
   router_->post(base + std::string("/attach"), bind(&RestServer::attach, this));
   router_->post(base + std::string("/detach"), bind(&RestServer::detach, this));
@@ -230,19 +233,32 @@ void RestServer::logJson(json j) {
 
 void RestServer::root_handler(const Pistache::Rest::Request &request,
                               Pistache::Http::ResponseWriter response) {
-  HelpType help_type;
   auto help = request.query().get("help").getOrElse("NO_HELP");
   if (help == "NO_HELP") {
-    help_type = HelpType::NO_HELP;
-  } else if (help == "NONE") {
-    help_type = HelpType::NONE;
-  } else {
-    Rest::Server::ResponseGenerator::Generate(
-        std::vector<Response>{{ErrorTag::kInvalidValue, nullptr}},
-        std::move(response));
+    Rest::Server::ResponseGenerator::Generate({{kOk, nullptr}},
+                                              std::move(response));
     return;
   }
 
+  HelpType type;
+  if (help == "NONE") {
+    type = NONE;
+  } else {
+    Rest::Server::ResponseGenerator::Generate({{kBadRequest, nullptr}},
+                                              std::move(response));
+    return;
+  }
+
+  auto completion = request.query().has("completion");
+  if (!completion) {
+    return root_help(type, std::move(response));
+  } else {
+    return root_completion(type, std::move(response));
+  }
+}
+
+void RestServer::root_help(HelpType help_type,
+                           Pistache::Http::ResponseWriter response) {
   if (help_type == HelpType::NONE) {
     json j;
     auto services = core.get_servicectrls_list();
@@ -259,6 +275,37 @@ void RestServer::root_handler(const Pistache::Rest::Request &request,
     }
 
     response.send(Pistache::Http::Code::Ok, j.dump());
+  }
+}
+
+void RestServer::root_completion(HelpType help_type,
+                                 Pistache::Http::ResponseWriter response) {
+  if (help_type == HelpType::NONE) {
+    json val = json::array();
+
+    // fixed commands
+    val += "connect";
+    val += "disconnect";
+    val += "attach";
+    val += "detach";
+    val += "services";
+    val += "cubes";
+    val += "topology";
+    val += "netdevs";
+
+    // services
+    auto services = core.get_servicectrls_list();
+    for (auto &it : services) {
+      val += it->get_name();
+    }
+
+    // cubes
+    auto cubes =ServiceController::get_all_cubes();
+    for (auto &it : cubes) {
+      val += it->get_name();
+    }
+
+    response.send(Pistache::Http::Code::Ok, val.dump());
   }
 }
 
@@ -566,6 +613,36 @@ void RestServer::disconnect(const Pistache::Rest::Request &request,
     logger->error("{0}", e.what());
     response.send(Pistache::Http::Code::Bad_Request, e.what());
   }
+}
+
+void RestServer::connect_help(const Pistache::Rest::Request &request,
+                              Pistache::Http::ResponseWriter response) {
+  auto help = request.query().get("help").getOrElse("NO_HELP");
+  if (help != "NONE") {
+    response.send(Pistache::Http::Code::Bad_Request);
+    return;
+  }
+
+  auto completion = request.query().has("completion");
+  if (completion) {
+    return connect_completion("", std::move(response));
+  }
+
+  response.send(Pistache::Http::Code::Ok, "not implemented");;
+}
+
+void RestServer::connect_completion(const std::string &p1,
+                                    Pistache::Http::ResponseWriter response) {
+  auto ports = core.get_all_ports();
+
+  if (!p1.empty()) {
+    std::string p(p1);
+    //ports.erase(p);
+    ports.erase(std::remove(ports.begin(), ports.end(), p1), ports.end());
+  }
+
+  json val = ports;
+  response.send(Pistache::Http::Code::Ok, val.dump());
 }
 
 void RestServer::attach(const Pistache::Rest::Request &request,

commit 8a63ca187e5c894dc579c94b84a54a6add06ff80

polycubectl: fix autocompletion
A last change before merging introduced a bug that make it unusable.

Fixes: 95aa7415d741 ("implement bash autocompletion")

Signed-off-by: Mauricio Vasquez B <mauriciovasquezbernal@gmail.com>

src/polycubectl/completion/polycubectl.sh
@@ -23,7 +23,6 @@ parse_yaml() {
 [[ $PS1 && -f /usr/share/bash-completion/bash_completion ]] && \
     . /usr/share/bash-completion/bash_completion
 
-# TODO: Get this from configuration file and env variable
 BASE_URL_="http://localhost:9000/polycube/v1/"
 
 _polycubectl_completions() {
@@ -33,7 +32,7 @@ _polycubectl_completions() {
   fi
 
   BASE_URL=${POLYCUBECTL_URL:-$config_url}
-  BASE_URL=${BASE_URL:-BASE_URL_}
+  BASE_URL=${BASE_URL:-$BASE_URL_}
   local cur prev
   _get_comp_words_by_ref -n ":,=" cur prev
   local words