polycube-network/polycube

commit e09356c4c03367c86f26435b8a01d548f54a0b32

services: remove generate_code and generate_code_vector
In most of the cases those functions only return a constant variable,
so remove them and use the variable directly.

Signed-off-by: Mauricio Vasquez B <mauriciovasquezbernal@gmail.com>

src/services/pcn-k8switch/src/K8switch.cpp
@@ -24,7 +24,7 @@ using namespace polycube::service;
 using namespace Tins;
 
 K8switch::K8switch(const std::string name, const K8switchJsonObject &conf)
-    : Cube(conf.getBase(), {generate_code()}, {}) {
+    : Cube(conf.getBase(), {}, {}) {
   logger()->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [K8switch] [%n] [%l] %v");
   logger()->info("Creating K8switch instance");
 
@@ -123,23 +123,6 @@ K8switchJsonObject K8switch::toJsonObject() {
   //  conf.addFwdTable(i->toJsonObject());
   //}
   return conf;
-}
-
-std::string K8switch::generate_code() {
-  // this code is compiled on contruction, return stub code that compiles
-  // fast, anyway this will be updated soon
-  const std::string stub_k8switch_code = R"POLYCUBE_DP(
-  static __always_inline
-  int handle_rx(struct CTXTYPE *ctx, struct pkt_metadata *md) {
-    return RX_DROP;
-  }
-  )POLYCUBE_DP";
-
-  return stub_k8switch_code;
-}
-
-std::vector<std::string> K8switch::generate_code_vector() {
-  throw std::runtime_error("Method not implemented");
 }
 
 void K8switch::packet_in(Ports &port, polycube::service::PacketInMetadata &md,

src/services/pcn-k8switch/src/K8switch.h
@@ -66,8 +66,6 @@ class K8switch : public polycube::service::Cube<Ports>,
  public:
   K8switch(const std::string name, const K8switchJsonObject &conf);
   virtual ~K8switch();
-  std::string generate_code();
-  std::vector<std::string> generate_code_vector();
   void packet_in(Ports &port, polycube::service::PacketInMetadata &md,
                  const std::vector<uint8_t> &packet) override;
 

src/services/pcn-ddosmitigator/src/Ddosmitigator.cpp
@@ -23,7 +23,7 @@ using namespace polycube::service;
 
 Ddosmitigator::Ddosmitigator(const std::string name,
                              const DdosmitigatorJsonObject &conf)
-    : TransparentCube(conf.getBase(), {generate_code()}, {}) {
+    : TransparentCube(conf.getBase(), {ddosmitigator_code}, {}) {
   logger()->info("Creating Ddosmitigator instance {0}", name);
 
   auto value = conf.getStats();
@@ -86,14 +86,6 @@ void Ddosmitigator::packet_in(polycube::service::Sense sense,
                               polycube::service::PacketInMetadata &md,
                               const std::vector<uint8_t> &packet) {
   logger()->info("packet in event");
-}
-
-std::string Ddosmitigator::generate_code() {
-  return ddosmitigator_code;
-}
-
-std::vector<std::string> Ddosmitigator::generate_code_vector() {
-  throw std::runtime_error("Method not implemented");
 }
 
 void Ddosmitigator::replaceAll(std::string &str, const std::string &from,

src/services/pcn-ddosmitigator/src/Ddosmitigator.h
@@ -35,8 +35,6 @@ class Ddosmitigator : public polycube::service::TransparentCube,
  public:
   Ddosmitigator(const std::string name, const DdosmitigatorJsonObject &conf);
   virtual ~Ddosmitigator();
-  std::string generate_code();
-  std::vector<std::string> generate_code_vector();
 
   void update(const DdosmitigatorJsonObject &conf) override;
   DdosmitigatorJsonObject toJsonObject() override;

src/services/pcn-firewall/src/Firewall.cpp
@@ -18,7 +18,7 @@
 #include "Firewall_dp.h"
 
 Firewall::Firewall(const std::string name, const FirewallJsonObject &conf)
-    : Cube(conf.getBase(), {generate_code()}, {}) {
+    : Cube(conf.getBase(), {firewall_code}, {}) {
   logger()->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [Firewall] [%n] [%l] %v");
   logger()->info("Creating Firewall instance");
 
@@ -147,14 +147,6 @@ FirewallJsonObject Firewall::toJsonObject() {
   conf.setInteractive(getInteractive());
 
   return conf;
-}
-
-std::string Firewall::generate_code() {
-  return firewall_code;
-}
-
-std::vector<std::string> Firewall::generate_code_vector() {
-  throw std::runtime_error("Method not implemented");
 }
 
 void Firewall::packet_in(Ports &port, polycube::service::PacketInMetadata &md,

src/services/pcn-firewall/src/Firewall.h
@@ -66,8 +66,6 @@ class Firewall : public polycube::service::Cube<Ports>,
  public:
   Firewall(const std::string name, const FirewallJsonObject &conf);
   virtual ~Firewall();
-  std::string generate_code();
-  std::vector<std::string> generate_code_vector();
   void packet_in(Ports &port, polycube::service::PacketInMetadata &md,
                  const std::vector<uint8_t> &packet) override;
 

src/services/pcn-helloworld/src/Helloworld.cpp
@@ -25,7 +25,7 @@
 using namespace polycube::service;
 
 Helloworld::Helloworld(const std::string name, const HelloworldJsonObject &conf)
-    : Cube(conf.getBase(), {generate_code()}, {}) {
+    : Cube(conf.getBase(), {helloworld_code_ingress}, {}) {
   logger()->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [helloworld] [%n] [%l] %v");
   logger()->info("Creating helloworld instance");
 
@@ -82,14 +82,6 @@ HelloworldJsonObject Helloworld::toJsonObject() {
   }
 
   return conf;
-}
-
-std::string Helloworld::generate_code() {
-  return helloworld_code_ingress;
-}
-
-std::vector<std::string> Helloworld::generate_code_vector() {
-  throw std::runtime_error("Method not implemented");
 }
 
 void Helloworld::packet_in(Ports &port, polycube::service::PacketInMetadata &md,

src/services/pcn-helloworld/src/Helloworld.h
@@ -35,8 +35,6 @@ class Helloworld : public polycube::service::Cube<Ports>,
  public:
   Helloworld(const std::string name, const HelloworldJsonObject &conf);
   virtual ~Helloworld();
-  std::string generate_code();
-  std::vector<std::string> generate_code_vector();
   void packet_in(Ports &port, polycube::service::PacketInMetadata &md,
                  const std::vector<uint8_t> &packet) override;
 

commit 7176fa441777d2d3cf34ef28367727edb4a74df2

pcn-k8switch: fix bad initialization logic
Before 09356c4c033 ("services: remove generate_code and generate_code_vector")
the k8switch was created with a stub program and then reloaded with the initial
configuration.  That approach is far from optimal as the code is immediately
reloaded, then 09356c4c033 changed that to created the cube without any eBPF
program and then add it with the initial configuration.

However, the call to reload_program was still there, it fails because the
stub is not present anymore.  This commits fix that by calling add_program
when the cube is created.

Fixes: e09356c4c033 ("services: remove generate_code and generate_code_vector")

Signed-off-by: Mauricio Vasquez B <mauriciovasquezbernal@gmail.com>

src/services/pcn-k8switch/src/K8switch.cpp
@@ -34,7 +34,7 @@ K8switch::K8switch(const std::string name, const K8switchJsonObject &conf)
   doSetVirtualClientSubnet(conf.getVirtualClientSubnet());
 
   // reload code a single time
-  reloadConfig();
+  add_program(getFlags() + k8switch_code, 0);
 
   addServiceList(conf.getService());
   addPortsList(conf.getPorts());
@@ -170,7 +170,7 @@ void K8switch::doSetVirtualClientSubnet(const std::string &value) {
   virtual_client_cidr_ = value;
 }
 
-void K8switch::reloadConfig() {
+std::string K8switch::getFlags() {
   std::string flags;
 
   // ports
@@ -197,6 +197,13 @@ void K8switch::reloadConfig() {
       "#define CLIENT_SUBNET " + std::to_string(htonl(client_subnet_)) + "\n";
   flags += "#define VIRTUAL_CLIENT_SUBNET " +
            std::to_string(htonl(virtual_client_subnet_)) + "\n";
+  logger()->debug("flags is {}", flags);
+
+  return flags;
+}
+
+void K8switch::reloadConfig() {
+  std::string flags(getFlags());
 
   logger()->debug("Reloading code with flags port: {}", flags);
 

src/services/pcn-k8switch/src/K8switch.h
@@ -143,6 +143,7 @@ class K8switch : public polycube::service::Cube<Ports>,
   void cleanupSessionTable();
   uint32_t timestampToAge(const uint64_t timestamp);
   void tick();
+  std::string getFlags();
 
   std::unique_ptr<std::thread> tick_thread;
   bool stop_;