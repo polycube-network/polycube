polycube-network/polycube

commit d8d280cd96a49ec4b6706e9ed3eef32c127c108f

Added new REST API and validation code
Signed-off-by: Nico Caprioli <nico.caprioli@gmail.com>

src/polycubed/src/server/Resources/Body/ListResource.cpp
@@ -1,0 +1,112 @@
+/*
+ * Copyright 2018 The Polycube Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "ListResource.h"
+
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#include "../../Validators/ValueValidator.h"
+#include "JsonNodeField.h"
+#include "ListKey.h"
+
+namespace polycube::polycubed::Rest::Resources::Body {
+ListResource::ListResource(const std::string &name,
+                           const std::string &description,
+                           const std::string &cli_example,
+                           const Body::ParentResource *parent,
+                           PolycubedCore *core, std::vector<ListKey> &&keys,
+                           const std::vector<JsonNodeField> &node_fields,
+                           bool configuration, bool init_only_config)
+    : ParentResource(name, description, cli_example, parent, core, node_fields,
+                     configuration, init_only_config, false),
+      keys_{std::move(keys)} {}
+
+bool ListResource::ValidateKeys(
+    std::unordered_map<std::string, std::string> keys) const {
+  for (const auto &key : keys_) {
+    if (keys.count(key.Name()) == 0)
+      return false;
+    const auto &value = keys.at(key.Name());
+    for (const auto &validator : key.Validators()) {
+      if (!validator->Validate(value))
+        return false;
+    }
+  }
+  return true;
+}
+
+const Response ListResource::ReadValue(const std::string &cube_name,
+                                       const ListKeyValues &keys) const {
+  const auto &parent_value = Resource::ReadValue(cube_name, keys);
+  auto jvalue = nlohmann::json::parse(parent_value.message)[name_];
+  ListKeyValues current_keys;
+  for (const auto &key : keys_) {
+    const auto nk = std::find_if(
+        std::begin(keys), std::end(keys),
+        [=](const ListKeyValue &k) { return k.name == key.Name(); });
+    if (nk != std::end(keys)) {
+      current_keys.push_back(*nk);
+    }
+  }
+  for (auto &element : jvalue) {
+    unsigned matching_keys = 0;
+    for (const auto &key : current_keys) {
+      if (element[key.name] == key.value) {
+        matching_keys += 1;
+      }
+      if (matching_keys == keys_.size())
+        return {ErrorTag::kOk, ::strdup(element.dump().data())};
+    }
+  }
+  throw std::runtime_error(
+      "Unreachable since keys are mandatory and validated.");
+}
+
+const Response ListResource::ReadWhole(const std::string &cube_name,
+                                       const ListKeyValues &keys) const {
+  return ParentResource::ReadValue(cube_name, keys);
+}
+
+void ListResource::SetDefaultIfMissing(nlohmann::json &body,
+                                       bool initialization) const {
+  // keys default values must be ignored (RFC7950#7.8.2)
+  std::set<std::string> key_names;
+  for (const auto &key : keys_) {
+    key_names.emplace(key.Name());
+  }
+
+  for (const auto &child : children_) {
+    // Set default only for configuration nodes. During initialization
+    // all non-keys can be defaulted, otherwise only the ones that are not
+    // marked as init-only-config.
+    if (key_names.count(child->Name()) == 0 && child->IsConfiguration() &&
+        (initialization || !child->IsInitOnlyConfig())) {
+      child->SetDefaultIfMissing(body[child->Name()], initialization);
+      if (body[child->Name()].empty()) {
+        body.erase(child->Name());
+      }
+    }
+  }
+}
+
+bool ListResource::IsMandatory() const {
+  return false;
+}
+
+}  // namespace polycube::polycubed::Rest::Resources::Body

src/polycubed/src/server/Resources/Body/ListResource.h
@@ -1,0 +1,53 @@
+/*
+ * Copyright 2018 The Polycube Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include "ParentResource.h"
+
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+namespace polycube::polycubed::Rest::Resources::Body {
+class ListKey;
+
+class ListResource : public virtual ParentResource {
+ public:
+  ListResource(const std::string &name, const std::string &description,
+               const std::string &cli_example,
+               const ParentResource *const parent, PolycubedCore *core,
+               std::vector<ListKey> &&keys,
+               const std::vector<JsonNodeField> &node_fields,
+               bool configuration, bool init_only_config);
+
+  bool ValidateKeys(std::unordered_map<std::string, std::string> keys) const;
+
+  const Response ReadValue(const std::string &cube_name,
+                           const ListKeyValues &keys) const override;
+
+  virtual const Response ReadWhole(const std::string &cube_name,
+                                   const ListKeyValues &keys) const;
+
+  bool IsMandatory() const override;
+
+  void SetDefaultIfMissing(nlohmann::json &body,
+                           bool initialization) const final;
+
+  const std::vector<ListKey> keys_;
+};
+}  // namespace polycube::polycubed::Rest::Resources::Body

src/polycubed/src/server/Resources/Body/ParentResource.cpp
@@ -1,0 +1,181 @@
+/*
+ * Copyright 2018 The Polycube Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "ParentResource.h"
+
+#include <algorithm>
+#include <list>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "ChoiceResource.h"
+#include "JsonNodeField.h"
+#include "ListResource.h"
+
+namespace polycube::polycubed::Rest::Resources::Body {
+
+ParentResource::ParentResource(
+    const std::string &name, const std::string &description,
+    const std::string &cli_example, const Body::ParentResource *parent,
+    PolycubedCore *core, const std::vector<JsonNodeField> &node_fields,
+    bool configuration, bool init_only_config, bool container_presence,
+    bool rpc_action)
+    : Resource(name, description, cli_example, parent, core, configuration,
+               init_only_config, node_fields),
+      children_{},
+      container_presence_(container_presence),
+      rpc_action_(rpc_action) {}
+
+std::vector<Response> ParentResource::BodyValidate(const std::string &cube_name,
+                                                   const ListKeyValues &keys,
+                                                   nlohmann::json &body,
+                                                   bool initialization) const {
+  std::vector<Response> errors;
+
+  for (const auto &node_field : node_fields_) {
+    const auto node_validation = node_field.Validate(this, cube_name, keys);
+    if (node_validation != kOk) {
+      errors.push_back({node_validation, ::strdup(name_.data())});
+      return errors;
+    }
+  }
+
+  std::vector<std::shared_ptr<Resource>> choices;
+  auto body_copy = body;
+  for (auto &child : children_) {
+    const auto &child_name = child->Name();
+    // Choices are a special case
+    if (std::dynamic_pointer_cast<ChoiceResource>(child) != nullptr) {
+      choices.push_back(child);
+    } else {
+      // if children is not present in request body
+      if (body_copy.count(child_name) == 0) {
+        // but it is required (both mandatory and method = POST or PUT
+        if (initialization && child->IsMandatory()) {
+          errors.push_back(
+              {ErrorTag::kMissingAttribute, ::strdup(child_name.data())});
+        }
+      } else {
+        // non configuration nodes are read only;
+        // same applies for configuration nodes marked as init-only-config
+        // during an operation different from initialization
+        if (!child->IsConfiguration() ||
+            (!initialization && child->IsInitOnlyConfig())) {
+          errors.push_back(
+              {ErrorTag::kInvalidValue, ::strdup(child_name.data())});
+        } else {
+          auto child_errors = child->BodyValidate(
+              cube_name, keys, body_copy[child_name], initialization);
+          errors.reserve(errors.size() + child_errors.size());
+          // remove current parsed element from the body so that in the
+          // eventuality of choices, they will operate only on unparsed
+          // elements. moreover, this is required for detecting unparsed
+          // elements, that may be typos
+          body_copy.erase(child->Name());
+          std::move(std::begin(child_errors), std::end(child_errors),
+                    std::back_inserter(errors));
+        }
+      }
+    }
+  }
+
+  for (const auto &choice : choices) {
+    // notice: body_copy is stripped of elements correctly parsed
+    // by the current choice (if any)
+    auto choice_errors =
+        choice->BodyValidate(cube_name, keys, body_copy, initialization);
+    // the method will return a single UnparsableChoice if it was not
+    // mandatory and it could not match any case.
+    if (choice_errors[0].error_tag != ErrorTag::kUnparsableChoice) {
+      errors.reserve(errors.size() + choice_errors.size());
+      std::move(std::begin(choice_errors), std::end(choice_errors),
+                std::back_inserter(errors));
+    }
+  }
+
+  errors.reserve(errors.size() + body_copy.size());
+  // body_copy should be empty as all items must be correctly parsed. If
+  // it is not, then there is an error in the input data
+  for (auto &unparsed : body_copy.items()) {
+    errors.push_back(
+        {ErrorTag::kInvalidValue, ::strdup(unparsed.key().data())});
+  }
+
+  return errors;
+}
+
+void ParentResource::AddChild(std::shared_ptr<Resource> &&child) {
+  children_.push_back(std::move(child));
+}
+
+bool ParentResource::IsMandatory() const {
+  if (container_presence_)
+    return true;
+  if (rpc_action_)
+    return false;
+  return std::end(children_) !=
+         std::find_if(std::begin(children_), std::end(children_),
+                      [](const std::shared_ptr<Resource> &child) {
+                        return child->IsMandatory();
+                      });
+}
+
+bool ParentResource::IsConfiguration() const {
+  return configuration_ ||
+         std::end(children_) !=
+             std::find_if(std::begin(children_), std::end(children_),
+                          [](const std::shared_ptr<Resource> &child) {
+                            return child->IsConfiguration();
+                          });
+}
+
+void ParentResource::SetDefaultIfMissing(nlohmann::json &body,
+                                         bool initialization) const {
+  for (const auto &child : children_) {
+    auto &child_body = body[child->Name()];
+    // Lists can't be defaulted. The only possible this is if a list
+    // (as json array) is provided in request body. In this situation
+    // each element of the array can be defaulted.
+    if (std::dynamic_pointer_cast<ListResource>(child) != nullptr) {
+      if (!child_body.empty()) {
+        for (auto &entry : child_body) {
+          child->SetDefaultIfMissing(entry, initialization);
+        }
+      }
+    } else {
+      // Set default only for configuration nodes. During initialization
+      // all can be defaulted, otherwise only the ones that are not marked
+      // as init-only-config
+      if (child->IsConfiguration() &&
+          (initialization || !child->IsInitOnlyConfig())) {
+        child->SetDefaultIfMissing(child_body, initialization);
+      }
+    }
+    if (child_body.empty()) {
+      body.erase(child->Name());
+    }
+  }
+}
+
+std::shared_ptr<Resource> ParentResource::Child(
+    const std::string &child_name) const {
+  auto child = std::find_if(
+      std::begin(children_), std::end(children_),
+      [=](std::shared_ptr<Resource> r) { return r->Name() == child_name; });
+  return (child == std::end(children_)) ? nullptr : *child;
+}
+}  // namespace polycube::polycubed::Rest::Resources::Body

src/polycubed/src/server/Resources/Endpoint/ParentResource.cpp
@@ -1,0 +1,248 @@
+/*
+ * Copyright 2018 The Polycube Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "ParentResource.h"
+
+#include <algorithm>
+#include <memory>
+#include <string>
+#include <utility>
+
+#include "rest_server.h"
+
+#include "../../Server/ResponseGenerator.h"
+#include "../Body/JsonNodeField.h"
+#include "../Body/ListKey.h"
+#include "Service.h"
+
+namespace polycube::polycubed::Rest::Resources::Endpoint {
+ParentResource::ParentResource(
+    const std::string &name, const std::string &description,
+    const std::string &cli_example, const std::string &rest_endpoint,
+    const Body::ParentResource *parent, PolycubedCore *core,
+    const std::vector<Body::JsonNodeField> &node_fields, bool configuration,
+    bool init_only_config, bool container_presence, bool rpc_action)
+    : Body::ParentResource(name, description, cli_example, parent, core,
+                           node_fields, configuration, init_only_config,
+                           container_presence, rpc_action),
+      Endpoint::Resource(rest_endpoint),
+      has_endpoints_(true),
+      rpc_action_(rpc_action) {
+  using Pistache::Rest::Routes::bind;
+  auto router = core_->get_rest_server()->get_router();
+  router->options(rest_endpoint_, bind(&ParentResource::options, this));
+  if (rpc_action_) {
+    router->post(rest_endpoint_, bind(&ParentResource::post, this));
+  } else {
+    router->get(rest_endpoint_, bind(&ParentResource::get, this));
+    if (configuration_ && !init_only_config_) {
+      router->post(rest_endpoint_, bind(&ParentResource::post, this));
+      router->put(rest_endpoint_, bind(&ParentResource::put, this));
+      router->patch(rest_endpoint_, bind(&ParentResource::patch, this));
+      router->del(rest_endpoint_, bind(&ParentResource::del, this));
+    }
+  }
+}
+
+ParentResource::~ParentResource() {
+  if (has_endpoints_) {
+    using Pistache::Http::Method;
+    auto router = core_->get_rest_server()->get_router();
+    router->removeRoute(Method::Options, rest_endpoint_);
+    if (rpc_action_) {
+      router->removeRoute(Method::Post, rest_endpoint_);
+    } else {
+      router->removeRoute(Method::Get, rest_endpoint_);
+      if (configuration_ && !init_only_config_) {
+        router->removeRoute(Method::Post, rest_endpoint_);
+        router->removeRoute(Method::Put, rest_endpoint_);
+        router->removeRoute(Method::Patch, rest_endpoint_);
+        router->removeRoute(Method::Delete, rest_endpoint_);
+      }
+    }
+  }
+}
+
+void ParentResource::CreateReplaceUpdate(
+    const Pistache::Rest::Request &request,
+    Pistache::Http::ResponseWriter response, bool update, bool initialization) {
+  std::vector<Response> errors;
+  if (parent_ != nullptr) {
+    auto rerrors =
+        dynamic_cast<const ParentResource *const>(parent_)->RequestValidate(
+            request, name_);
+    errors.reserve(rerrors.size());
+    std::move(std::begin(rerrors), std::end(rerrors),
+              std::back_inserter(errors));
+  }
+
+  nlohmann::json jbody;
+  if (request.body().empty()) {
+    jbody = nlohmann::json::parse("{}");
+  } else {
+    jbody = nlohmann::json::parse(request.body());
+  }
+
+  // Mandatory value is checked only during POST and PUT, both
+  // considered as initialization operations.
+  SetDefaultIfMissing(jbody, initialization);
+
+  const auto &cube_name = Service::Cube(request);
+  ListKeyValues keys{};
+  Keys(request, keys);
+  auto body_errors = BodyValidate(cube_name, keys, jbody, initialization);
+  errors.reserve(errors.size() + body_errors.size());
+  std::move(std::begin(body_errors), std::end(body_errors),
+            std::back_inserter(errors));
+
+  if (errors.empty()) {
+    auto op = OperationType(update, initialization);
+    auto resp = WriteValue(cube_name, jbody, keys, op);
+    if (resp.error_tag == ErrorTag::kOk) {
+      errors.push_back({ErrorTag::kCreated, nullptr});
+    } else {
+      errors.push_back(resp);
+    }
+  }
+  Server::ResponseGenerator::Generate(std::move(errors), std::move(response));
+}
+
+std::vector<Response> ParentResource::RequestValidate(
+    const Pistache::Rest::Request &request,
+    [[maybe_unused]] const std::string &caller_name) const {
+  std::vector<Response> errors;
+  if (parent_ != nullptr) {
+    auto rerrors =
+        dynamic_cast<const ParentResource *const>(parent_)->RequestValidate(
+            request, name_);
+    errors.reserve(rerrors.size());
+    std::move(std::begin(rerrors), std::end(rerrors),
+              std::back_inserter(errors));
+  }
+  return errors;
+}
+
+void ParentResource::Keys(const Pistache::Rest::Request &request,
+                          ListKeyValues &parsed) const {
+  if (parent_ != nullptr) {
+    // Do not call Keys on Service
+    if (dynamic_cast<const Service *const>(parent_) == nullptr) {
+      dynamic_cast<const ParentResource *const>(parent_)->Keys(request, parsed);
+    }
+  }
+}
+
+void ParentResource::get(const Request &request, ResponseWriter response) {
+  std::vector<Response> errors;
+  if (parent_ != nullptr) {
+    auto rerrors =
+        dynamic_cast<const ParentResource *const>(parent_)->RequestValidate(
+            request, name_);
+    errors.reserve(rerrors.size());
+    std::move(std::begin(rerrors), std::end(rerrors),
+              std::back_inserter(errors));
+  }
+  if (errors.empty()) {
+    const auto &cube_name = Service::Cube(request);
+    ListKeyValues keys{};
+    if (parent_ != nullptr) {
+      Keys(request, keys);
+    }
+    errors.push_back(ReadValue(cube_name, keys));
+  }
+  Server::ResponseGenerator::Generate(std::move(errors), std::move(response));
+}
+
+void ParentResource::post(const Request &request, ResponseWriter response) {
+  CreateReplaceUpdate(request, std::move(response), false, true);
+}
+
+void ParentResource::put(const Request &request, ResponseWriter response) {
+  CreateReplaceUpdate(request, std::move(response), true, true);
+}
+
+void ParentResource::patch(const Request &request, ResponseWriter response) {
+  CreateReplaceUpdate(request, std::move(response), true, false);
+}
+
+void ParentResource::del(const Request &request, ResponseWriter response) {
+  auto errors = RequestValidate(request, name_);
+  if (!errors.empty()) {
+    Server::ResponseGenerator::Generate(std::move(errors), std::move(response));
+    return;
+  }
+  const auto &cube_name = Service::Cube(request);
+  ListKeyValues keys{};
+  Keys(request, keys);
+  if (ReadValue(cube_name, keys).error_tag != ErrorTag::kOk) {
+    Server::ResponseGenerator::Generate(
+        std::vector<Response>{{ErrorTag::kDataMissing, nullptr}},
+        std::move(response));
+    return;
+  }
+  auto resp = DeleteValue(cube_name, keys);
+  if (resp.error_tag == ErrorTag::kOk) {
+    Server::ResponseGenerator::Generate(
+        std::vector<Response>{{ErrorTag::kNoContent, nullptr}},
+        std::move(response));
+    errors.push_back({ErrorTag::kCreated, nullptr});
+  } else {
+    Server::ResponseGenerator::Generate(std::vector<Response>{resp},
+                                        std::move(response));
+  }
+}
+
+void ParentResource::options(const Request &request, ResponseWriter response) {
+  const auto &query_param = request.query();
+  if (!query_param.has("help")) {
+    Server::ResponseGenerator::Generate({{kBadRequest, nullptr}},
+                                        std::move(response));
+    return;
+  }
+
+  auto help = query_param.get("help").get();
+  if (help == "NO_HELP") {
+    Server::ResponseGenerator::Generate({{kOk, nullptr}}, std::move(response));
+  }
+
+  HelpType type;
+  if (help == "SHOW") {
+    type = SHOW;
+  } else if (help == "SET") {
+    type = SET;
+  } else if (help == "NONE") {
+    type = NONE;
+  } else {
+    Server::ResponseGenerator::Generate({{kBadRequest, nullptr}},
+                                        std::move(response));
+    return;
+  }
+  ListKeyValues keys{};
+  Keys(request, keys);
+
+  Server::ResponseGenerator::Generate(
+      {Help(Service::Cube(request), type, keys)}, std::move(response));
+}
+
+ParentResource::ParentResource(
+    const std::string &name, const std::string &description,
+    const std::string &cli_example, const Body::ParentResource *parent,
+    PolycubedCore *core, const std::vector<Body::JsonNodeField> &node_fields)
+    : Body::ParentResource(name, description, cli_example, nullptr, nullptr,
+                           node_fields, false, false, false),
+      Endpoint::Resource(""),
+      has_endpoints_(false),
+      rpc_action_(false) {}
+}  // namespace polycube::polycubed::Rest::Resources::Endpoint

commit 18becf4588ce82a2ad2b8af50cafb853e8bc76ab

polycubed: fix list validation
The validation code for the list was not testing all the elements of the list,
it instead was testing the list as a container.  Due to this complex commands
issued with a rest client were failing, for instance:

curl -L -X POST --data '{"name":"hw0","ports":[{"name":"pippo"}]}' \
  "http://localhost:9000/polycube/v1/helloworld/hw0/"

curl -L -X POST --data \
  '[{"name":"toveth1", "peer":"veth1"}, {"name":"toveth2", "peer":"veth2"}]' \
  "http://localhost:9000/polycube/v1/helloworld/hw0/ports/

This commit implements a new logic that tests all the elements in a list, also
a new logic fill the keys in json body of a list request is added as well.

This failure was unnoticed because polycubectl does not allow to perform such
complex operations in a single command.

Fixes: d8d280cd96a4 ("Added new REST API and validation code")
Fixes: 795ce964c60c ("move help generation from services to daemon")

Signed-off-by: Mauricio Vasquez B <mauriciovasquezbernal@gmail.com>

src/polycubed/src/server/Resources/Body/ListResource.cpp
@@ -105,6 +105,46 @@ void ListResource::SetDefaultIfMissing(nlohmann::json &body,
   }
 }
 
+void ListResource::FillKeys(nlohmann::json &body, const ListKeyValues &keys) {
+  // TODO: is there a more efficient implementation of this?
+  for (auto &key: keys_) {
+    for (auto &kv : keys) {
+      if (key.Name() == kv.name) {
+        // TODO: check if the key is present in the body and compare the data
+        body[key.OriginalName()] = kv.value;
+        break;
+      }
+    }
+  }
+}
+
+std::vector<Response> ListResource::BodyValidateMultiple(
+    const std::string &cube_name, const ListKeyValues &keys,
+    nlohmann::json &body, bool initialization) const {
+  std::vector<Response> errors;
+
+  nlohmann::json jbody;
+  if (body.empty()) {
+    jbody = nlohmann::json::parse("[]");
+  } else {
+    jbody = body;
+  }
+
+  if (!body.is_array()) {
+    errors.push_back({ErrorTag::kInvalidValue, nullptr});
+    return errors;
+  }
+
+  for (auto &elem : jbody) {
+    SetDefaultIfMissing(elem, initialization);
+    auto body = BodyValidate(cube_name, keys, elem, initialization);
+    errors.reserve(errors.size() + body.size());
+    std::copy(std::begin(body), std::end(body), std::back_inserter(errors));
+  }
+
+  return errors;
+}
+
 bool ListResource::IsMandatory() const {
   return false;
 }

src/polycubed/src/server/Resources/Body/ListResource.h
@@ -48,6 +48,16 @@ class ListResource : public virtual ParentResource {
   void SetDefaultIfMissing(nlohmann::json &body,
                            bool initialization) const final;
 
+  /*
+   * This function takes the keys (parsed from the url in "keys") and save
+   * them in the body of the request
+   */
+  void FillKeys(nlohmann::json &body, const ListKeyValues &keys);
+
+  std::vector<Response> BodyValidateMultiple(
+      const std::string &cube_name, const ListKeyValues &keys,
+      nlohmann::json &body, bool initialization) const;
+
   const std::vector<ListKey> keys_;
 
   nlohmann::json ToHelpJson() const override;

src/polycubed/src/server/Resources/Body/ParentResource.cpp
@@ -61,11 +61,6 @@ std::vector<Response> ParentResource::BodyValidate(const std::string &cube_name,
   for (auto &child : children_) {
     const auto &child_name = child->Name();
 
-    // TODO: what are the implications of it?
-    if (child->IsKey()) {
-      continue;
-    }
-
     // Choices are a special case
     if (std::dynamic_pointer_cast<ChoiceResource>(child) != nullptr) {
       choices.push_back(child);
@@ -86,9 +81,18 @@ std::vector<Response> ParentResource::BodyValidate(const std::string &cube_name,
           errors.push_back(
               {ErrorTag::kInvalidValue, ::strdup(child_name.data())});
         } else {
-          auto child_errors = child->BodyValidate(
-              cube_name, keys, body_copy[child_name], initialization);
+          std::vector<Response> child_errors;
+          // if the child is a list, perform the check over all elements
+          if (auto list = std::dynamic_pointer_cast<ListResource>(child)) {
+            child_errors = list->BodyValidateMultiple(
+                cube_name, keys, body_copy[child_name], initialization);
+          } else {
+            child_errors = child->BodyValidate(
+                cube_name, keys, body_copy[child_name], initialization);
+          }
+
           errors.reserve(errors.size() + child_errors.size());
+
           // remove current parsed element from the body so that in the
           // eventuality of choices, they will operate only on unparsed
           // elements. moreover, this is required for detecting unparsed

src/polycubed/src/server/Resources/Endpoint/ParentResource.cpp
@@ -105,6 +105,12 @@ void ParentResource::CreateReplaceUpdate(
   const auto &cube_name = Service::Cube(request);
   ListKeyValues keys{};
   Keys(request, keys);
+
+  // fill keys if they are missing
+  if (auto list = dynamic_cast<ListResource*>(this)) {
+    list->FillKeys(jbody, keys);
+  }
+
   auto body_errors = BodyValidate(cube_name, keys, jbody, initialization);
   errors.reserve(errors.size() + body_errors.size());
   std::move(std::begin(body_errors), std::end(body_errors),