polycube-network/polycube

commit 95aa7415d741166fd3b30af56a2c685ae3888567

implement bash autocompletion
bash completion speeds up the writing of long commands, it also helps the
user with the different available options for a given command.

Signed-off-by: Mauricio Vasquez B <mauriciovasquezbernal@gmail.com>

src/polycubed/src/server/Resources/Endpoint/ListResource.cpp
@@ -56,8 +56,18 @@ ListResource::ListResource(const std::string &name,
   }
   auto router = core_->get_rest_server()->get_router();
   router->get(multiple_endpoint_, bind(&ListResource::get_multiple, this));
+
+  // endpoints for help
   router->options(multiple_endpoint_,
                   bind(&ListResource::options_multiple, this));
+
+  std::string endpoint(multiple_endpoint_ + "/");
+  for (int i = 0; i < key_params_.size() - 1; i++) {
+    endpoint += key_params_.at(i).Name() + "/";
+    router->options(endpoint,
+                    bind(&ListResource::options_multiple, this));
+  }
+
   if (configuration_ && !init_only_config) {
     router->post(multiple_endpoint_, bind(&ListResource::post_multiple, this));
     router->put(multiple_endpoint_, bind(&ListResource::put_multiple, this));
@@ -72,6 +82,13 @@ ListResource::~ListResource() {
   auto router = core_->get_rest_server()->get_router();
   router->removeRoute(Method::Get, multiple_endpoint_);
   router->removeRoute(Method::Options, multiple_endpoint_);
+
+  std::string endpoint(multiple_endpoint_ + "/");
+  for (int i = 0; i < key_params_.size() - 1; i++) {
+    endpoint += key_params_.at(i).Name() + "/";
+    router->removeRoute(Method::Options, endpoint);
+  }
+
   if (configuration_ && !init_only_config_) {
     router->removeRoute(Method::Post, multiple_endpoint_);
     router->removeRoute(Method::Put, multiple_endpoint_);
@@ -100,6 +117,16 @@ void ListResource::Keys(const Pistache::Rest::Request &request,
         {k.Name(), k.Type(), request.param(':' + k.Name()).as<std::string>()});
   }
   dynamic_cast<const ParentResource *const>(parent_)->Keys(request, parsed);
+}
+
+void ListResource::GetListKeys(const Pistache::Rest::Request &request,
+                               ListKeyValues &parsed) const {
+  for (const auto &k : keys_) {
+    if (request.hasParam(':' + k.Name())) {
+      parsed.push_back(
+          {k.Name(), k.Type(), request.param(':' + k.Name()).as<std::string>()});
+    }
+  }
 }
 
 void ListResource::CreateReplaceUpdateWhole(
@@ -236,8 +263,16 @@ void ListResource::options_multiple(const Request &request,
   ListKeyValues keys{};
 
   dynamic_cast<const ParentResource *const>(parent_)->Keys(request, keys);
-  auto helpresp = Help(Service::Cube(request), type, keys);
-  Server::ResponseGenerator::Generate({helpresp}, std::move(response));
+
+  if (!query_param.has("completion")) {
+    auto resp = Help(Service::Cube(request), type, keys);
+    Server::ResponseGenerator::Generate({resp}, std::move(response));
+  } else {
+    ListKeyValues list_keys{};
+    GetListKeys(request, list_keys);
+    auto resp = Completion(Service::Cube(request), type, keys, list_keys);
+    Server::ResponseGenerator::Generate({resp}, std::move(response));
+  }
 }
 
 Response ListResource::Help(const std::string &cube_name, HelpType type,
@@ -282,6 +317,59 @@ nlohmann::json ListResource::helpKeys() const {
   }
 
   return val;
+}
+
+Response ListResource::Completion(const std::string &cube_name, HelpType type,
+                                  const ListKeyValues &keys,
+                                  const ListKeyValues &list_keys) {
+  nlohmann::json elements_json =
+      nlohmann::json::parse(GetElementsList(cube_name, keys).message);
+
+  auto size_keys = list_keys.size();
+  std::string keyname = keys_[size_keys].Name();
+
+  nlohmann::json val = nlohmann::json::array();
+
+  if (type == HelpType::ADD) {
+    val = {"<" + keyname + ">"};
+    return {kOk, ::strdup(val.dump().c_str())};
+  }
+
+  // This is a nice library but a silly approach to convert it back.
+  std::vector<nlohmann::fifo_map<std::string, std::string>> elements = elements_json;
+
+  for (auto &item : elements) {
+    // check if element matches giving key
+    bool found = true;
+    for (auto &key : list_keys) {
+      if (item.at(key.name) != key.value) {
+        found = false;
+        break;
+      }
+    }
+
+    if (found) {
+      val += item.at(keyname);
+    }
+  }
+
+  switch (type) {
+  case HelpType::SHOW:
+    break;
+  case HelpType::DEL:
+    break;
+  case HelpType::NONE:
+    if (configuration_) {
+      val += "add";
+      val += "del";
+    }
+    val += "show";
+    break;
+  default:
+    return {kBadRequest, nullptr};
+  }
+
+  return {kOk, ::strdup(val.dump().c_str())};
 }
 
 }  // namespace polycube::polycubed::Rest::Resources::Endpoint

scripts/install.sh
@@ -82,6 +82,7 @@ PACKAGES+=" autoconf libtool m4 automake"
 PACKAGES+=" libssl-dev" # needed for certificate based security
 PACKAGES+=" sudo" # needed for pcn-iptables, when building docker image
 PACKAGES+=" kmod" # needed for pcn-iptables, when using lsmod to unload conntrack if not needed
+PACKAGES+=" jq" # needed for polycubectl bash autocompletion
 
 if [ "$MODE" == "pcn-k8s" ]; then
   PACKAGES+=" curl" # needed for pcn-k8s to download a binary

src/polycubectl/CMakeLists.txt
@@ -2,3 +2,34 @@ configure_file(version.go.in ${CMAKE_CURRENT_SOURCE_DIR}//version.go)
 install(CODE
 "execute_process( COMMAND /bin/sh -c \"GOPATH=$ENV{GOPATH} USER=$ENV{USER} ${CMAKE_SOURCE_DIR}/src/polycubectl/install.sh\"
 WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/src/polycubectl/ )")
+
+
+# this is the right approach but it is not working on ubuntu, for some reason
+# the cmake package is not installed in ubuntu
+
+#find_package(bash-completion)
+#if(BASH_COMPLETION_FOUND)
+#  message(STATUS
+#    "Using bash completion dir ${BASH_COMPLETION_COMPLETIONSDIR}")
+#else()
+#  set (BASH_COMPLETION_COMPLETIONSDIR "/etc/bash_completion.d")
+#  message (STATUS
+#    "Using fallback bash completion dir ${BASH_COMPLETION_COMPLETIONSDIR}")
+#endif()
+#
+#install(completion/polycubectl.sh polycubectl DESTINATION
+#  ${BASH_COMPLETION_COMPLETIONSDIR})
+
+# get the path were to install the completion scripts
+# more info (https://github.com/scop/bash-completion)
+execute_process (
+  COMMAND pkg-config --variable=completionsdir bash-completion
+  OUTPUT_VARIABLE COMPLETION_PATH
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+install(
+  FILES completion/polycubectl.sh
+  DESTINATION ${COMPLETION_PATH}
+  RENAME polycubectl
+)

src/polycubectl/completion/polycubectl.sh
@@ -1,0 +1,158 @@
+#/usr/bin/env bash
+
+parse_yaml() {
+   local prefix=$2
+   local s='[[:space:]]*' w='[a-zA-Z0-9_]*' fs=$(echo @|tr @ '\034')
+   sed -ne "s|^\($s\)\($w\)$s:$s\"\(.*\)\"$s\$|\1$fs\2$fs\3|p" \
+        -e "s|^\($s\)\($w\)$s:$s\(.*\)$s\$|\1$fs\2$fs\3|p"  $1 |
+   awk -F$fs '{
+      indent = length($1)/2;
+      vname[indent] = $2;
+      for (i in vname) {if (i > indent) {delete vname[i]}}
+      if (length($3) > 0) {
+         vn=""; for (i=0; i<indent; i++) {vn=(vn)(vname[i])("_")}
+         printf("%s%s%s=\"%s\"\n", "'$prefix'",vn, $2, $3);
+      }
+   }'
+}
+
+# Use bash-completion, if available
+[[ $PS1 && -f /usr/share/bash-completion/bash_completion ]] && \
+    . /usr/share/bash-completion/bash_completion
+
+# TODO: Get this from configuration file and env variable
+BASE_URL_="http://localhost:9000/polycube/v1/"
+
+_polycubectl_completions() {
+  if [ -a "$HOME/.config/polycube/polycubectl_config.yaml" ]
+  then
+    eval $(parse_yaml "$HOME/.config/polycube/polycubectl_config.yaml" "config_")
+  fi
+
+  BASE_URL=${POLYCUBECTL_URL:-$config_url}
+  BASE_URL=${BASE_URL:-BASE_URL_}
+  local cur prev
+  _get_comp_words_by_ref -n ":,=" cur prev
+  local words
+  _get_comp_words_by_ref -n ":,=" -w words
+
+  OLDIFS=$IFS
+  COMP=""
+
+  URL0=""
+  HELP_TYPE="NONE"
+  i=0
+  for X in ${words[@]}
+  do
+    let i++
+
+    # do not consider the current word the user is typing
+    #if [ $i == $((COMP_CWORD+1)) ]
+    if [ "$X" == "$cur" ]
+    then
+      #break
+      continue
+    fi
+
+    if [[ $X == *"="* ]]
+    then
+      continue
+    fi
+
+    if [[ $X == *":"* ]]
+    then
+      continue
+    fi
+
+    if [[ "polycubectl" == $X ]]
+    then
+      continue
+    fi
+
+    if [[ "add" == $X || "del" == $X || "show" == $X || "set" == $X ]]
+    then
+      HELP_TYPE=`echo $X | awk '{print toupper($0)}'`
+      continue
+    fi
+
+    #echo $X
+    URL0+=$X
+    URL0+='/'
+  done
+
+  #if [ "${cur: -1}" == "=" ]
+  if [[ $cur == *"="* ]]
+  then
+    URL0+="${cur%%=*}"
+    URL0+='/'
+  fi
+
+  URL0="${URL0%?}"  # remove last '/'
+  URL=$BASE_URL$URL0"?help="$HELP_TYPE"&completion"
+
+  #echo "URL is: " $URL
+  #return
+
+  JSON=`curl -f -L -s -X OPTIONS $URL`
+  if [ $? -ne 0 ]
+  then
+    return
+  fi
+
+  #echo $JSON
+
+  VEC="`echo $JSON | jq -cr '.? // [] | @tsv'`"
+
+  if [ ${#VEC[@]} == 1 ]
+  then
+    VEC0=${VEC[0]}
+    if [[ ${VEC0:0:1} == "<" ]]
+    then
+      # if the command is ADD then print the keyname that is expected, but
+      # without creating a completion
+      printf "\n${VEC0}" >/dev/tty
+      expandedPrompt=$(PS1="$PS1" debian_chroot="$debian_chroot" "$BASH" --norc -i </dev/null 2>&1 | sed -n '${s/^\(.*\)exit$/\1/p;}')
+      printf '\n%s%s' "$expandedPrompt" "$COMP_LINE" >/dev/tty
+      return 0;
+    fi
+  fi
+
+  IFS=$'\t'
+  for CMD in ${VEC[@]}
+  do
+    # add space for commands that doesn't end in "="
+    if [ ${CMD: -1} != "=" ]
+    then
+      CMD+=" "
+    fi
+
+    if [[ $cur == *":"* ]]
+    then
+      CMD="${CMD#*:}"
+    fi
+
+    COMP+="$CMD"$IFS
+  done
+
+  IFS=$'\t\n'
+
+  local cur0=$cur
+
+  # if "=" or ":" are present in the current word, get the substring after it,
+  # otherwise the completion is going to give wrong results
+  if [[ $cur == *"="* ]]
+  then
+    cur0="${cur#*=}"
+  fi
+
+  if [[ $cur == *":"* ]]
+  then
+    cur0="${cur#*:}"
+  fi
+
+  COMPREPLY=( $(compgen -W "$COMP" -- $cur0) )
+
+  IFS=$OLDIFS
+}
+
+complete -o nospace -F _polycubectl_completions polycubectl

src/polycubed/src/cube.cpp
@@ -153,6 +153,10 @@ std::shared_ptr<PortIface> Cube::get_port(const std::string &name) {
   return ports_by_name_.at(name);
 }
 
+std::map<std::string, std::shared_ptr<PortIface>> &Cube::get_ports() {
+  return ports_by_name_;
+}
+
 void Cube::update_forwarding_table(int index, int value) {
   std::lock_guard<std::mutex> cube_guard(cube_mutex_);
   if (forward_chain_)  // is the forward chain still active?

src/polycubed/src/cube.h
@@ -59,6 +59,7 @@ class Cube : public BaseCube, public CubeIface {
                                       const nlohmann::json &conf);
   void remove_port(const std::string &name);
   std::shared_ptr<PortIface> get_port(const std::string &name);
+  std::map<std::string, std::shared_ptr<PortIface>> &get_ports();
 
   void update_forwarding_table(int index, int value);
 

src/polycubed/src/polycubed_core.cpp
@@ -478,6 +478,23 @@ std::string PolycubedCore::set_cube_parameter(const std::string &cube,
       "PolycubedCore::set_cube_parameter is not implemented");
 }
 
+std::vector<std::string> PolycubedCore::get_all_ports() {
+  std::vector<std::string> ports;
+  auto cubes = ServiceController::get_all_cubes();
+  for (auto &cube_ : cubes) {
+    auto cube = std::dynamic_pointer_cast<Cube>(cube_);
+    if (!cube) {
+      continue;
+    }
+
+    for (auto &it : cube->get_ports()) {
+      ports.push_back(cube->get_name() + ":" + it.first);
+    }
+  }
+
+  return std::move(ports);
+}
+
 void PolycubedCore::set_rest_server(RestServer *rest_server) {
   rest_server_ = rest_server;
 }

src/polycubed/src/polycubed_core.h
@@ -79,6 +79,8 @@ class PolycubedCore {
                                  const std::string &parameter,
                                  const std::string &value);
 
+  std::vector<std::string> get_all_ports();
+
   void set_rest_server(RestServer *rest_server);
   RestServer *get_rest_server();
   BaseModel *base_model();

commit 3a890a8ecb4556fcf151d5c3528a3633edbeaba0

polycubed: fix list autocompletion
List autocompletion was partially broken because a look up with the wrong key
name was done.

This commit also adds some logic to avoid suggesting the "add", "del", "show"
commands when in the middle of a list completion. ex:

polycubectl cube_name complex_list key1 <tab>:  suggest list of key2 for
elements that also matchs key1.

Fixes: 95aa7415d741 ("implement bash autocompletion")

Signed-off-by: Mauricio Vasquez B <mauriciovasquezbernal@gmail.com>

src/polycubed/src/server/Resources/Endpoint/ListResource.cpp
@@ -327,6 +327,7 @@ Response ListResource::Completion(const std::string &cube_name, HelpType type,
 
   auto size_keys = list_keys.size();
   std::string keyname = keys_[size_keys].Name();
+  std::string original_keyname = keys_[size_keys].OriginalName();
 
   nlohmann::json val = nlohmann::json::array();
 
@@ -349,7 +350,7 @@ Response ListResource::Completion(const std::string &cube_name, HelpType type,
     }
 
     if (found) {
-      val += item.at(keyname);
+      val += item.at(original_keyname);
     }
   }
 
@@ -359,11 +360,14 @@ Response ListResource::Completion(const std::string &cube_name, HelpType type,
   case HelpType::DEL:
     break;
   case HelpType::NONE:
-    if (configuration_) {
-      val += "add";
-      val += "del";
+    // suggest these commands only if we are not in the middle of a list
+    if (list_keys.size() == 0) {
+      if (configuration_) {
+        val += "add";
+        val += "del";
+      }
+      val += "show";
     }
-    val += "show";
     break;
   default:
     return {kBadRequest, nullptr};