polycube-network/polycube

commit 18becf4588ce82a2ad2b8af50cafb853e8bc76ab

polycubed: fix list validation
The validation code for the list was not testing all the elements of the list,
it instead was testing the list as a container.  Due to this complex commands
issued with a rest client were failing, for instance:

curl -L -X POST --data '{"name":"hw0","ports":[{"name":"pippo"}]}' \
  "http://localhost:9000/polycube/v1/helloworld/hw0/"

curl -L -X POST --data \
  '[{"name":"toveth1", "peer":"veth1"}, {"name":"toveth2", "peer":"veth2"}]' \
  "http://localhost:9000/polycube/v1/helloworld/hw0/ports/

This commit implements a new logic that tests all the elements in a list, also
a new logic fill the keys in json body of a list request is added as well.

This failure was unnoticed because polycubectl does not allow to perform such
complex operations in a single command.

Fixes: d8d280cd96a4 ("Added new REST API and validation code")
Fixes: 795ce964c60c ("move help generation from services to daemon")

Signed-off-by: Mauricio Vasquez B <mauriciovasquezbernal@gmail.com>

src/polycubed/src/server/Resources/Body/ListResource.cpp
@@ -105,6 +105,46 @@ void ListResource::SetDefaultIfMissing(nlohmann::json &body,
   }
 }
 
+void ListResource::FillKeys(nlohmann::json &body, const ListKeyValues &keys) {
+  // TODO: is there a more efficient implementation of this?
+  for (auto &key: keys_) {
+    for (auto &kv : keys) {
+      if (key.Name() == kv.name) {
+        // TODO: check if the key is present in the body and compare the data
+        body[key.OriginalName()] = kv.value;
+        break;
+      }
+    }
+  }
+}
+
+std::vector<Response> ListResource::BodyValidateMultiple(
+    const std::string &cube_name, const ListKeyValues &keys,
+    nlohmann::json &body, bool initialization) const {
+  std::vector<Response> errors;
+
+  nlohmann::json jbody;
+  if (body.empty()) {
+    jbody = nlohmann::json::parse("[]");
+  } else {
+    jbody = body;
+  }
+
+  if (!body.is_array()) {
+    errors.push_back({ErrorTag::kInvalidValue, nullptr});
+    return errors;
+  }
+
+  for (auto &elem : jbody) {
+    SetDefaultIfMissing(elem, initialization);
+    auto body = BodyValidate(cube_name, keys, elem, initialization);
+    errors.reserve(errors.size() + body.size());
+    std::copy(std::begin(body), std::end(body), std::back_inserter(errors));
+  }
+
+  return errors;
+}
+
 bool ListResource::IsMandatory() const {
   return false;
 }

src/polycubed/src/server/Resources/Endpoint/ParentResource.cpp
@@ -105,6 +105,12 @@ void ParentResource::CreateReplaceUpdate(
   const auto &cube_name = Service::Cube(request);
   ListKeyValues keys{};
   Keys(request, keys);
+
+  // fill keys if they are missing
+  if (auto list = dynamic_cast<ListResource*>(this)) {
+    list->FillKeys(jbody, keys);
+  }
+
   auto body_errors = BodyValidate(cube_name, keys, jbody, initialization);
   errors.reserve(errors.size() + body_errors.size());
   std::move(std::begin(body_errors), std::end(body_errors),

src/polycubed/src/server/Resources/Body/ListResource.h
@@ -48,6 +48,16 @@ class ListResource : public virtual ParentResource {
   void SetDefaultIfMissing(nlohmann::json &body,
                            bool initialization) const final;
 
+  /*
+   * This function takes the keys (parsed from the url in "keys") and save
+   * them in the body of the request
+   */
+  void FillKeys(nlohmann::json &body, const ListKeyValues &keys);
+
+  std::vector<Response> BodyValidateMultiple(
+      const std::string &cube_name, const ListKeyValues &keys,
+      nlohmann::json &body, bool initialization) const;
+
   const std::vector<ListKey> keys_;
 
   nlohmann::json ToHelpJson() const override;

src/polycubed/src/server/Resources/Body/ParentResource.cpp
@@ -61,11 +61,6 @@ std::vector<Response> ParentResource::BodyValidate(const std::string &cube_name,
   for (auto &child : children_) {
     const auto &child_name = child->Name();
 
-    // TODO: what are the implications of it?
-    if (child->IsKey()) {
-      continue;
-    }
-
     // Choices are a special case
     if (std::dynamic_pointer_cast<ChoiceResource>(child) != nullptr) {
       choices.push_back(child);
@@ -86,9 +81,18 @@ std::vector<Response> ParentResource::BodyValidate(const std::string &cube_name,
           errors.push_back(
               {ErrorTag::kInvalidValue, ::strdup(child_name.data())});
         } else {
-          auto child_errors = child->BodyValidate(
-              cube_name, keys, body_copy[child_name], initialization);
+          std::vector<Response> child_errors;
+          // if the child is a list, perform the check over all elements
+          if (auto list = std::dynamic_pointer_cast<ListResource>(child)) {
+            child_errors = list->BodyValidateMultiple(
+                cube_name, keys, body_copy[child_name], initialization);
+          } else {
+            child_errors = child->BodyValidate(
+                cube_name, keys, body_copy[child_name], initialization);
+          }
+
           errors.reserve(errors.size() + child_errors.size());
+
           // remove current parsed element from the body so that in the
           // eventuality of choices, they will operate only on unparsed
           // elements. moreover, this is required for detecting unparsed

commit 7683f8af29b41aca17e87cd973723dcfc357b508

polycubed: further fix to list validation
18becf4588ce fixed some problems with the list validation, however this also
introduced another problems as the that fix was not complete.

This commit adds some logic to fill the keys in a list request according to its
type.

Fixes: 18becf4588ce ("polycubed: fix list validation")

Signed-off-by: Mauricio Vasquez B <mauriciovasquezbernal@gmail.com>

src/polycubed/src/server/Resources/Body/ListResource.cpp
@@ -25,6 +25,8 @@
 #include "JsonNodeField.h"
 #include "ListKey.h"
 
+#include "../../Types/lexical_cast.h"
+
 namespace polycube::polycubed::Rest::Resources::Body {
 ListResource::ListResource(const std::string &name,
                            const std::string &description,
@@ -107,11 +109,41 @@ void ListResource::SetDefaultIfMissing(nlohmann::json &body,
 
 void ListResource::FillKeys(nlohmann::json &body, const ListKeyValues &keys) {
   // TODO: is there a more efficient implementation of this?
-  for (auto &key: keys_) {
+  for (auto &key : keys_) {
     for (auto &kv : keys) {
       if (key.Name() == kv.name) {
         // TODO: check if the key is present in the body and compare the data
-        body[key.OriginalName()] = kv.value;
+        switch (key.Type()) {
+        case ListType::kBool:
+          body[key.OriginalName()] = Types::lexical_cast<bool>(kv.value);
+          break;
+        case ListType::kInt8:
+          body[key.OriginalName()] = Types::lexical_cast<int8_t>(kv.value);
+          break;
+        case ListType::kInt16:
+          body[key.OriginalName()] = Types::lexical_cast<int16_t>(kv.value);
+          break;
+        case ListType::kInt32:
+          body[key.OriginalName()] = Types::lexical_cast<int32_t>(kv.value);
+          break;
+        case ListType::kInt64:
+          body[key.OriginalName()] = Types::lexical_cast<int64_t>(kv.value);
+          break;
+        case ListType::kUint8:
+          body[key.OriginalName()] = Types::lexical_cast<uint8_t>(kv.value);
+          break;
+        case ListType::kUint16:
+          body[key.OriginalName()] = Types::lexical_cast<uint16_t>(kv.value);
+          break;
+        case ListType::kUint32:
+          body[key.OriginalName()] = Types::lexical_cast<uint32_t>(kv.value);
+          break;
+        case ListType::kUint64:
+          body[key.OriginalName()] = Types::lexical_cast<uint64_t>(kv.value);
+          break;
+        default:
+          body[key.OriginalName()] = kv.value;
+        }
         break;
       }
     }

src/polycubed/src/server/Resources/Endpoint/ListResource.h
@@ -73,7 +73,7 @@ class ListResource : public ParentResource, public Body::ListResource {
  private:
   // try to get as much as possible Keys
   void GetListKeys(const Pistache::Rest::Request &request,
-                  ListKeyValues &parsed) const;
+                   ListKeyValues &parsed) const;
 
   std::vector<PathParamField> key_params_;
   std::string multiple_endpoint_;

src/polycubed/src/server/Resources/Endpoint/ParentResource.cpp
@@ -107,7 +107,7 @@ void ParentResource::CreateReplaceUpdate(
   Keys(request, keys);
 
   // fill keys if they are missing
-  if (auto list = dynamic_cast<ListResource*>(this)) {
+  if (auto list = dynamic_cast<ListResource *>(this)) {
     list->FillKeys(jbody, keys);
   }
 

src/polycubed/src/server/Types/lexical_cast.cpp
@@ -15,7 +15,30 @@
  */
 #include "lexical_cast.h"
 
+#include <algorithm>
+#include <string>
+
 namespace polycube::polycubed::Rest::Types {
+template <>
+bool lexical_cast<bool>(const std::string &value) {
+  try {
+    std::string lower;
+    lower.reserve(value.length());
+    std::transform(std::begin(value), std::end(value),
+                   std::back_inserter(lower),
+                   static_cast<int (*)(int)>(std::tolower));
+    if (lower == "false") {
+      return false;
+    } else if (lower == "true") {
+      return true;
+    } else {
+      throw bad_lexical_cast();
+    }
+  } catch (...) {
+    throw bad_lexical_cast();
+  }
+}
+
 template <>
 int8_t lexical_cast<int8_t>(const std::string &value) {
   try {

src/polycubed/src/server/Types/lexical_cast.h
@@ -26,6 +26,9 @@ template <class T>
 T lexical_cast(const std::string &value) = delete;
 
 template <>
+bool lexical_cast<bool>(const std::string &value);
+
+template <>
 int8_t lexical_cast<int8_t>(const std::string &value);
 
 template <>