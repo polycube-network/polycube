polycube-network/polycube

commit d8d280cd96a49ec4b6706e9ed3eef32c127c108f

Added new REST API and validation code
Signed-off-by: Nico Caprioli <nico.caprioli@gmail.com>

src/polycubed/src/server/Resources/Body/ChoiceResource.cpp
@@ -1,0 +1,101 @@
+/*
+ * Copyright 2018 The Polycube Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "ChoiceResource.h"
+
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "../../Validators/InSetValidator.h"
+#include "CaseResource.h"
+#include "JsonNodeField.h"
+
+namespace polycube::polycubed::Rest::Resources::Body {
+ChoiceResource::ChoiceResource(
+    const std::string &name, const std::string &description,
+    const std::string &cli_example, const Body::ParentResource *parent,
+    PolycubedCore *core, bool mandatory,
+    std::unique_ptr<const std::string> &&default_case)
+    : ParentResource(name, description, cli_example, parent, core,
+                     std::vector<JsonNodeField>{}, false, false, false),
+      mandatory_(mandatory),
+      default_case_(std::move(default_case)) {}
+
+std::vector<Response> ChoiceResource::BodyValidate(const std::string &cube_name,
+                                                   const ListKeyValues &keys,
+                                                   nlohmann::json &body,
+                                                   bool initialization) const {
+  bool parsed_child = false;
+  std::vector<Response> errors;
+  for (const auto &child : children_) {
+    const auto &child_name = child->Name();
+    std::vector<Response> child_errors;
+    // if children is choice or case, pass whole body
+    if (std::dynamic_pointer_cast<ChoiceResource>(child) != nullptr ||
+        std::dynamic_pointer_cast<CaseResource>(child) != nullptr) {
+      child_errors = child->BodyValidate(cube_name, keys, body, initialization);
+    } else {  // else pass to the correct child
+      if (body.count(child_name) == 0) {
+        if (initialization && IsMandatory() && child->IsMandatory()) {
+          errors.push_back(
+              {ErrorTag::kMissingAttribute, ::strdup(child_name.data())});
+        }
+      } else {
+        if (!child->IsConfiguration() ||
+            (!initialization && child->IsInitOnlyConfig())) {
+          errors.push_back({ErrorTag::kInvalidValue, nullptr});
+        } else {
+          child_errors =
+              child->BodyValidate(cube_name, keys, body.at(child_name),
+                                  initialization && IsMandatory());
+          errors.reserve(errors.size() + child_errors.size());
+          body.erase(child_name);
+          std::move(std::begin(child_errors), std::end(child_errors),
+                    std::back_inserter(errors));
+        }
+      }
+    }
+    if (child_errors.empty()) {
+      if (parsed_child && errors.empty()) {
+        errors.push_back({ErrorTag::kBadAttribute, ::strdup(name_.data())});
+      } else {
+        parsed_child = true;
+      }
+    }
+  }
+  if (errors.empty() || !parsed_child) {
+    errors.push_back({ErrorTag::kUnparsableChoice, nullptr});
+  }
+  return errors;
+}
+
+bool ChoiceResource::IsMandatory() const {
+  return mandatory_ &&
+         dynamic_cast<const CaseResource *const>(parent_) != nullptr;
+}
+
+void ChoiceResource::SetDefaultIfMissing(nlohmann::json &body,
+                                         bool initialization) const {
+  if (default_case_ != nullptr) {
+    auto def = std::find_if(
+        std::begin(children_), std::end(children_),
+        [this](const auto &child) { return child->Name() == *default_case_; });
+    // no check if def is end because it ensured by YANG validation process
+    (*def)->SetDefaultIfMissing(body, initialization);
+  }
+}
+}  // namespace polycube::polycubed::Rest::Resources::Body

src/polycubed/src/server/Resources/Body/ChoiceResource.h
@@ -1,0 +1,50 @@
+/*
+ * Copyright 2018 The Polycube Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "ParentResource.h"
+
+namespace polycube::polycubed::Rest::Resources::Body {
+class ChoiceResource : public virtual ParentResource {
+ public:
+  ChoiceResource(const std::string &name, const std::string &description,
+                 const std::string &cli_example,
+                 const ParentResource *const parent, PolycubedCore *core,
+                 bool mandatory,
+                 std::unique_ptr<const std::string> &&default_case);
+
+  std::vector<Response> BodyValidate(const std::string &cube_name,
+                                     const ListKeyValues &keys,
+                                     nlohmann::json &body,
+                                     bool initialization) const final;
+
+  using ParentResource::AddChild;
+
+  bool IsMandatory() const final;
+
+  void SetDefaultIfMissing(nlohmann::json &body,
+                           bool initialization) const final;
+
+ private:
+  const bool mandatory_;
+  const std::unique_ptr<const std::string> default_case_;
+};
+}  // namespace polycube::polycubed::Rest::Resources::Body

src/polycubed/src/server/Resources/Body/LeafListResource.cpp
@@ -1,0 +1,76 @@
+/*
+ * Copyright 2018 The Polycube Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "LeafListResource.h"
+
+#include <algorithm>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "JsonNodeField.h"
+#include "JsonValueField.h"
+
+namespace polycube::polycubed::Rest::Resources::Body {
+
+LeafListResource::LeafListResource(
+    const std::string &name, const std::string &description,
+    const std::string &cli_example, const Body::ParentResource *parent,
+    PolycubedCore *core, std::unique_ptr<JsonValueField> &&value_field,
+    const std::vector<JsonNodeField> &node_fields, bool configuration,
+    bool init_only_config, bool mandatory, Types::Scalar type,
+    std::vector<std::string> &&default_value)
+    : LeafResource(name, description, cli_example, parent, core,
+                   std::move(value_field), node_fields, configuration,
+                   init_only_config, mandatory, type, nullptr),
+      default_(std::move(default_value)) {}
+
+std::vector<Response> LeafListResource::BodyValidate(
+    const std::string &cube_name, const ListKeyValues &keys,
+    nlohmann::json &body, bool initialization) const {
+  std::vector<Response> errors;
+  if (body.type() != nlohmann::detail::value_t::array) {
+    errors.push_back({ErrorTag::kBadAttribute, ::strdup(name_.data())});
+    return errors;
+  }
+
+  for (const auto &node_field : node_fields_) {
+    const auto node_validation = node_field.Validate(this, cube_name, keys);
+    if (node_validation != kOk) {
+      errors.push_back({node_validation, ::strdup(name_.data())});
+      return errors;
+    }
+  }
+
+  for (auto &element : body.items()) {
+    const auto &inner_errors = LeafResource::BodyValidate(
+        cube_name, keys, element.value(), initialization);
+    errors.reserve(errors.size() + inner_errors.size());
+    std::move(std::begin(inner_errors), std::end(inner_errors),
+              std::back_inserter(errors));
+  }
+  return errors;
+}
+
+void LeafListResource::SetDefaultIfMissing(nlohmann::json &body,
+                                           bool initialization) const {
+  if (body.empty() && !default_.empty()) {
+    for (const auto &element : default_) {
+      body.push_back(element);
+    }
+  }
+}
+}  // namespace polycube::polycubed::Rest::Resources::Body

src/polycubed/src/server/Resources/Body/LeafListResource.h
@@ -1,0 +1,49 @@
+/*
+ * Copyright 2018 The Polycube Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <memory>
+#include <string>
+#include <typeindex>
+#include <vector>
+#include "LeafResource.h"
+
+namespace polycube::polycubed::Rest::Resources::Body {
+class LeafListResource : public virtual LeafResource {
+ public:
+  LeafListResource(const std::string &name, const std::string &description,
+                   const std::string &cli_example,
+                   const ParentResource *const parent, PolycubedCore *core,
+                   std::unique_ptr<JsonValueField> &&value_field,
+                   const std::vector<JsonNodeField> &node_fields,
+                   bool configuration, bool init_only_config, bool mandatory,
+                   Types::Scalar type,
+                   std::vector<std::string> &&default_value);
+
+  ~LeafListResource() override = default;
+
+  std::vector<Response> BodyValidate(const std::string &cube_name,
+                                     const ListKeyValues &keys,
+                                     nlohmann::json &body,
+                                     bool initialization) const final;
+
+  void SetDefaultIfMissing(nlohmann::json &body,
+                           bool initialization) const final;
+
+ private:
+  const std::vector<std::string> default_;
+};
+}  // namespace polycube::polycubed::Rest::Resources::Body

src/polycubed/src/server/Resources/Body/LeafResource.cpp
@@ -1,0 +1,88 @@
+/*
+ * Copyright 2018 The Polycube Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "LeafResource.h"
+#include "JsonNodeField.h"
+#include "JsonValueField.h"
+
+namespace polycube::polycubed::Rest::Resources::Body {
+using Pistache::Http::ResponseWriter;
+using Pistache::Rest::Request;
+
+LeafResource::LeafResource(
+    const std::string &name, const std::string &description,
+    const std::string &cli_example, const ParentResource *const parent,
+    PolycubedCore *core, std::unique_ptr<JsonValueField> &&value_field,
+    const std::vector<JsonNodeField> &node_fields, bool configuration,
+    bool init_only_config, bool mandatory, Types::Scalar type,
+    std::unique_ptr<const std::string> &&default_value)
+    : Resource(name, description, cli_example, parent, core, configuration,
+               init_only_config, node_fields),
+      value_field_{std::move(value_field)},
+      mandatory_(mandatory),
+      type_(type),
+      default_{std::move(default_value)} {}
+
+std::vector<Response> LeafResource::BodyValidate(const std::string &cube_name,
+                                                 const ListKeyValues &keys,
+                                                 nlohmann::json &body,
+                                                 bool initialization) const {
+  std::vector<Response> errors;
+  if (body.empty()) {
+    errors.push_back({ErrorTag::kMissingAttribute, ::strdup(name_.data())});
+    return errors;
+  }
+
+  for (const auto &node_field : node_fields_) {
+    const auto node_validation = node_field.Validate(this, cube_name, keys);
+    if (node_validation != kOk) {
+      errors.push_back({node_validation, ::strdup(name_.data())});
+      return errors;
+    }
+  }
+
+  auto value_field = value_field_->Validate(body);
+  if (value_field != kOk) {
+    errors.push_back({value_field, ::strdup(name_.data())});
+  }
+  return errors;
+}
+
+void LeafResource::SetDefaultIfMissing(nlohmann::json &body,
+                                       bool initialization) const {
+  if (body.empty() && default_ != nullptr) {
+    switch (type_) {
+    case Types::Scalar::Integer:
+      body = std::stoll(*default_);
+      break;
+    case Types::Scalar::Unsigned:
+      body = std::stoull(*default_);
+      break;
+    case Types::Scalar::Decimal:
+      body = std::stod(*default_);
+      break;
+    case Types::Scalar::String:
+      body = *default_;
+      break;
+    case Types::Scalar::Boolean:
+      body = *default_ == "true";
+      break;
+    case Types::Scalar::Empty:
+      body = {nullptr};
+    }
+  }
+}
+}  // namespace polycube::polycubed::Rest::Resources::Body

src/polycubed/src/server/Resources/Body/LeafResource.h
@@ -1,0 +1,66 @@
+/*
+ * Copyright 2018 The Polycube Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <pistache/router.h>
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include "../../Types/Scalar.h"
+#include "Resource.h"
+#include "polycube/services/response.h"
+
+namespace polycube::polycubed::Rest::Resources::Body {
+class JsonValueField;
+
+class LeafResource : public Resource {
+ public:
+  LeafResource(const std::string &name, const std::string &description,
+               const std::string &cli_example,
+               const ParentResource *const parent, PolycubedCore *core,
+               std::unique_ptr<JsonValueField> &&value_fields,
+               const std::vector<JsonNodeField> &node_fields,
+               bool configuration, bool init_only_config, bool mandatory,
+               Types::Scalar type,
+               std::unique_ptr<const std::string> &&default_value);
+
+  ~LeafResource() override = default;
+
+  std::vector<Response> BodyValidate(const std::string &cube_name,
+                                     const ListKeyValues &keys,
+                                     nlohmann::json &body,
+                                     bool initialization) const override;
+
+  bool IsMandatory() const final {
+    return mandatory_;
+  }
+
+  bool IsConfiguration() const final {
+    return configuration_;
+  }
+
+  void SetDefaultIfMissing(nlohmann::json &body,
+                           bool initialization) const override;
+
+ protected:
+  const std::unique_ptr<JsonValueField> value_field_;
+  const bool mandatory_;
+  const Types::Scalar type_;
+  const std::unique_ptr<const std::string> default_;
+};
+}  // namespace polycube::polycubed::Rest::Resources::Body

commit c590168899cbedf2a39678e0af85c1a2c6bf8e0b

polycubed: set missing default elements only at initialization time
polycubed fills the default elements on a request if they are missing, this
guarantees that the default elements are always set when the request arrives to
the service implementation, hence the developer has not to take care of it.

When an update (PATCH) request is done those fields should not be set, because
their value will be modified even if the client wanted to update other elements.

Fixes: d8d280cd96a4 ("Added new REST API and validation code")

Signed-off-by: Mauricio Vasquez B <mauriciovasquezbernal@gmail.com>

src/polycubed/src/server/Resources/Body/ChoiceResource.cpp
@@ -88,14 +88,13 @@ bool ChoiceResource::IsMandatory() const {
          dynamic_cast<const CaseResource *const>(parent_) != nullptr;
 }
 
-void ChoiceResource::SetDefaultIfMissing(nlohmann::json &body,
-                                         bool initialization) const {
+void ChoiceResource::SetDefaultIfMissing(nlohmann::json &body) const {
   if (default_case_ != nullptr) {
     auto def = std::find_if(
         std::begin(children_), std::end(children_),
         [this](const auto &child) { return child->Name() == *default_case_; });
     // no check if def is end because it ensured by YANG validation process
-    (*def)->SetDefaultIfMissing(body, initialization);
+    (*def)->SetDefaultIfMissing(body);
   }
 }
 }  // namespace polycube::polycubed::Rest::Resources::Body

src/polycubed/src/server/Resources/Body/ChoiceResource.h
@@ -40,8 +40,7 @@ class ChoiceResource : public virtual ParentResource {
 
   bool IsMandatory() const final;
 
-  void SetDefaultIfMissing(nlohmann::json &body,
-                           bool initialization) const final;
+  void SetDefaultIfMissing(nlohmann::json &body) const final;
 
  private:
   const bool mandatory_;

src/polycubed/src/server/Resources/Body/LeafListResource.cpp
@@ -65,8 +65,7 @@ std::vector<Response> LeafListResource::BodyValidate(
   return errors;
 }
 
-void LeafListResource::SetDefaultIfMissing(nlohmann::json &body,
-                                           bool initialization) const {
+void LeafListResource::SetDefaultIfMissing(nlohmann::json &body) const {
   if (body.empty() && !default_.empty()) {
     for (const auto &element : default_) {
       body.push_back(element);

src/polycubed/src/server/Resources/Body/LeafListResource.h
@@ -40,8 +40,7 @@ class LeafListResource : public virtual LeafResource {
                                      nlohmann::json &body,
                                      bool initialization) const final;
 
-  void SetDefaultIfMissing(nlohmann::json &body,
-                           bool initialization) const final;
+  void SetDefaultIfMissing(nlohmann::json &body) const final;
 
  private:
   const std::vector<std::string> default_;

src/polycubed/src/server/Resources/Body/LeafResource.cpp
@@ -61,8 +61,7 @@ std::vector<Response> LeafResource::BodyValidate(const std::string &cube_name,
   return errors;
 }
 
-void LeafResource::SetDefaultIfMissing(nlohmann::json &body,
-                                       bool initialization) const {
+void LeafResource::SetDefaultIfMissing(nlohmann::json &body) const {
   if (body.empty() && default_ != nullptr) {
     switch (type_) {
     case Types::Scalar::Integer:

src/polycubed/src/server/Resources/Body/LeafResource.h
@@ -54,8 +54,7 @@ class LeafResource : public Resource {
     return configuration_;
   }
 
-  void SetDefaultIfMissing(nlohmann::json &body,
-                           bool initialization) const override;
+  void SetDefaultIfMissing(nlohmann::json &body) const override;
 
   nlohmann::json ToHelpJson() const override;
 

src/polycubed/src/server/Resources/Body/ListResource.cpp
@@ -85,8 +85,7 @@ const Response ListResource::ReadWhole(const std::string &cube_name,
   return ParentResource::ReadValue(cube_name, keys);
 }
 
-void ListResource::SetDefaultIfMissing(nlohmann::json &body,
-                                       bool initialization) const {
+void ListResource::SetDefaultIfMissing(nlohmann::json &body) const {
   // keys default values must be ignored (RFC7950#7.8.2)
   std::set<std::string> key_names;
   for (const auto &key : keys_) {
@@ -98,8 +97,8 @@ void ListResource::SetDefaultIfMissing(nlohmann::json &body,
     // all non-keys can be defaulted, otherwise only the ones that are not
     // marked as init-only-config.
     if (key_names.count(child->Name()) == 0 && child->IsConfiguration() &&
-        (initialization || !child->IsInitOnlyConfig())) {
-      child->SetDefaultIfMissing(body[child->Name()], initialization);
+        !child->IsInitOnlyConfig()) {
+      child->SetDefaultIfMissing(body[child->Name()]);
       if (body[child->Name()].empty()) {
         body.erase(child->Name());
       }
@@ -168,7 +167,9 @@ std::vector<Response> ListResource::BodyValidateMultiple(
   }
 
   for (auto &elem : jbody) {
-    SetDefaultIfMissing(elem, initialization);
+    if (initialization) {
+      SetDefaultIfMissing(elem);
+    }
     auto body = BodyValidate(cube_name, keys, elem, initialization);
     errors.reserve(errors.size() + body.size());
     std::copy(std::begin(body), std::end(body), std::back_inserter(errors));

src/polycubed/src/server/Resources/Body/ListResource.h
@@ -45,8 +45,7 @@ class ListResource : public virtual ParentResource {
 
   bool IsMandatory() const override;
 
-  void SetDefaultIfMissing(nlohmann::json &body,
-                           bool initialization) const final;
+  void SetDefaultIfMissing(nlohmann::json &body) const final;
 
   /*
    * This function takes the keys (parsed from the url in "keys") and save

src/polycubed/src/server/Resources/Body/ParentResource.cpp
@@ -155,8 +155,7 @@ bool ParentResource::IsConfiguration() const {
                           });
 }
 
-void ParentResource::SetDefaultIfMissing(nlohmann::json &body,
-                                         bool initialization) const {
+void ParentResource::SetDefaultIfMissing(nlohmann::json &body) const {
   for (const auto &child : children_) {
     auto &child_body = body[child->Name()];
     // Lists can't be defaulted. The only possible this is if a list
@@ -165,16 +164,13 @@ void ParentResource::SetDefaultIfMissing(nlohmann::json &body,
     if (std::dynamic_pointer_cast<ListResource>(child) != nullptr) {
       if (!child_body.empty()) {
         for (auto &entry : child_body) {
-          child->SetDefaultIfMissing(entry, initialization);
+          child->SetDefaultIfMissing(entry);
         }
       }
     } else {
-      // Set default only for configuration nodes. During initialization
-      // all can be defaulted, otherwise only the ones that are not marked
-      // as init-only-config
-      if (child->IsConfiguration() &&
-          (initialization || !child->IsInitOnlyConfig())) {
-        child->SetDefaultIfMissing(child_body, initialization);
+      // Set default only for configuration nodes.
+      if (child->IsConfiguration()) {
+        child->SetDefaultIfMissing(child_body);
       }
     }
     if (child_body.empty()) {

src/polycubed/src/server/Resources/Body/ParentResource.h
@@ -47,8 +47,7 @@ class ParentResource : public Resource {
 
   bool IsConfiguration() const final;
 
-  void SetDefaultIfMissing(nlohmann::json &body,
-                           bool initialization) const override;
+  void SetDefaultIfMissing(nlohmann::json &body) const override;
 
   std::shared_ptr<Resource> Child(const std::string &child_name) const;
 

src/polycubed/src/server/Resources/Body/Resource.h
@@ -87,8 +87,7 @@ class Resource {
                                              nlohmann::json &body,
                                              bool initialization) const = 0;
 
-  virtual void SetDefaultIfMissing(nlohmann::json &body,
-                                   bool initialization) const = 0;
+  virtual void SetDefaultIfMissing(nlohmann::json &body) const = 0;
 
   inline const Body::ParentResource *const Parent() const {
     return parent_;

src/polycubed/src/server/Resources/Endpoint/LeafResource.cpp
@@ -98,7 +98,7 @@ void LeafResource::CreateReplaceUpdate(const Pistache::Rest::Request &request,
   // This method can be reached only as direct call on the update method
   // of this LeafResource. It is possible only for configuration
   // nodes not marked as init-only-config.
-  SetDefaultIfMissing(jbody, false);
+  SetDefaultIfMissing(jbody);
 
   const auto cube_name = Service::Cube(request);
   ListKeyValues keys{};

src/polycubed/src/server/Resources/Endpoint/ListResource.cpp
@@ -160,7 +160,9 @@ void ListResource::CreateReplaceUpdateWhole(
   ListKeyValues keys{};
   dynamic_cast<const ParentResource *const>(parent_)->Keys(request, keys);
   for (auto &elem : jbody) {
-    SetDefaultIfMissing(elem, initialization);
+    if (initialization) {
+      SetDefaultIfMissing(elem);
+    }
     auto body = BodyValidate(cube_name, keys, elem, initialization);
     errors.reserve(errors.size() + body.size());
     std::copy(std::begin(body), std::end(body), std::back_inserter(errors));

src/polycubed/src/server/Resources/Endpoint/ParentResource.cpp
@@ -100,7 +100,9 @@ void ParentResource::CreateReplaceUpdate(
 
   // Mandatory value is checked only during POST and PUT, both
   // considered as initialization operations.
-  SetDefaultIfMissing(jbody, initialization);
+  if (initialization) {
+    SetDefaultIfMissing(jbody);
+  }
 
   const auto &cube_name = Service::Cube(request);
   ListKeyValues keys{};

src/polycubed/src/server/Resources/Endpoint/Service.cpp
@@ -73,7 +73,9 @@ void Service::CreateReplaceUpdate(const std::string &name, nlohmann::json &body,
   if (update || !ServiceController::exists_cube(name)) {
     auto op = OperationType(update, initialization);
     auto k = ListKeyValues{};
-    SetDefaultIfMissing(body, initialization);
+    if (initialization) {
+      SetDefaultIfMissing(body);
+    }
 
     auto jbody = body;
     jbody.erase("name");