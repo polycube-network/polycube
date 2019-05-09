polycube-network/polycube

commit 8884259e73382949a7faaf7d38eea4ce32a938ff

use json as a configuration mechanishm for cubes and ports
This commit removes from the services the logic to handle custom configuration
of cubes and ports.  The configuration is now passed to the framework as a
json object, there it is parsed and processed accordingly.

Signed-off-by: Mauricio Vasquez B <mauriciovasquezbernal@gmail.com>

src/polycubed/src/transparent_cube.cpp
@@ -141,5 +141,24 @@ void TransparentCube::send_packet_out(const std::vector<uint8_t> &packet,
   c.send_packet_to_cube(module, port, packet);
 }
 
+void TransparentCube::set_conf(const nlohmann::json &conf) {
+  return BaseCube::set_conf(conf);
+}
+
+nlohmann::json TransparentCube::to_json() const {
+  nlohmann::json j;
+  j.update(BaseCube::to_json());
+
+  std::string parent;
+  if (parent_) {
+    auto port_parent = dynamic_cast<Port *>(parent_);
+    parent = port_parent->get_path();
+  }
+
+  j["parent"] = parent;
+
+  return j;
+}
+
 }  // namespace polycubed
 }  // namespace polycube

src/libs/polycube/src/transparent_cube.cpp
@@ -21,11 +21,10 @@
 namespace polycube {
 namespace service {
 
-TransparentCube::TransparentCube(const std::string &name,
+TransparentCube::TransparentCube(const nlohmann::json &conf,
                                  const std::vector<std::string> &ingress_code,
-                                 const std::vector<std::string> &egress_code,
-                                 const CubeType type, LogLevel level)
-    : BaseCube(name, ingress_code, egress_code, type, level) {
+                                 const std::vector<std::string> &egress_code)
+    : BaseCube(conf, ingress_code, egress_code) {
   handle_packet_in = [&](const PacketIn *md,
                          const std::vector<uint8_t> &packet) -> void {
     // This lock guarantees:
@@ -44,8 +43,8 @@ TransparentCube::TransparentCube(const std::string &name,
   };
 
   cube_ = factory_->create_transparent_cube(
-      name, ingress_code, egress_code, handle_log_msg, type, handle_packet_in,
-      std::bind(&TransparentCube::attach, this), level);
+      conf, ingress_code, egress_code, handle_log_msg, handle_packet_in,
+      std::bind(&TransparentCube::attach, this));
   // TODO: where to keep this reference?, keep a double reference?
   BaseCube::cube_ = cube_;
 }
@@ -67,6 +66,14 @@ std::string TransparentCube::get_parent_parameter(
 void TransparentCube::send_packet_out(EthernetII &packet, Sense sense,
                                       bool recirculate) {
   cube_->send_packet_out(packet.serialize(), sense, recirculate);
+}
+
+void TransparentCube::set_conf(const nlohmann::json &conf) {
+  cube_->set_conf(conf);
+}
+
+nlohmann::json TransparentCube::to_json() const  {
+  return cube_->to_json();
 }
 
 void TransparentCube::attach() {}

src/libs/polycube/include/polycube/common.h
@@ -21,6 +21,14 @@
 
 namespace polycube {
 
+namespace service {
+  enum class CubeType;
+  enum class PortStatus;
+}
+
+using polycube::service::CubeType;
+using polycube::service::PortStatus;
+
 // Taken from
 // https://github.com/gabime/spdlog/blob/master/include/spdlog/common.h
 
@@ -34,6 +42,9 @@ enum class LogLevel {
   OFF = 6
 };
 
+// https://stackoverflow.com/a/4119881
+bool iequals(const std::string &a, const std::string &b);
+
 std::string logLevelString(polycube::LogLevel l);
 polycube::LogLevel stringLogLevel(const std::string &level);
 /* Log level representation in polycube and spdlog library could change,
@@ -41,6 +52,12 @@ polycube::LogLevel stringLogLevel(const std::string &level);
  * between the two representations
  */
 spdlog::level::level_enum logLevelToSPDLog(polycube::LogLevel level);
+
+std::string cube_type_to_string(CubeType type);
+CubeType string_to_cube_type(const std::string &type_str);
+
+std::string port_status_to_string(PortStatus status);
+PortStatus string_to_port_status(const std::string &status);
 
 size_t get_possible_cpu_count();
 

src/libs/polycube/include/polycube/services/base_cube.h
@@ -38,10 +38,9 @@ extern std::string logfile_;
 
 class BaseCube {
  public:
-  BaseCube(const std::string &name,
+  BaseCube(const nlohmann::json &conf,
            const std::vector<std::string> &ingress_code,
-           const std::vector<std::string> &egress_code, const CubeType type,
-           LogLevel level = LogLevel::OFF);
+           const std::vector<std::string> &egress_code);
   virtual ~BaseCube();
 
   // TODO: implement move constructor, forbid copy and asignment
@@ -85,6 +84,8 @@ class BaseCube {
   std::shared_ptr<spdlog::logger> logger();
 
   void dismount();
+
+  nlohmann::json to_json() const;
 
  protected:
   int get_table_fd(const std::string &table_name, int index, ProgramType type);

src/libs/polycube/include/polycube/services/cube.h
@@ -37,9 +37,8 @@ namespace service {
 template <class PortType>
 class Cube : public BaseCube {
  public:
-  Cube(const std::string &name, const std::vector<std::string> &ingress_code,
-       const std::vector<std::string> &egress_code, const CubeType type,
-       LogLevel level = LogLevel::OFF);
+  Cube(const nlohmann::json &conf, const std::vector<std::string> &ingress_code,
+       const std::vector<std::string> &egress_code);
   virtual ~Cube();
 
   template <class PortConfigType>
@@ -48,10 +47,13 @@ class Cube : public BaseCube {
   void remove_port(const std::string &port_name);
   std::shared_ptr<PortType> get_port(const std::string &port_name);
   std::shared_ptr<PortType> get_port(int index);
-  std::vector<std::shared_ptr<PortType>> get_ports();
+  std::vector<std::shared_ptr<PortType>> get_ports() const;
 
   virtual void packet_in(PortType &port, PacketInMetadata &md,
                          const std::vector<uint8_t> &packet) = 0;
+
+  void set_conf(const nlohmann::json &conf);
+  nlohmann::json to_json() const;
 
  private:
   std::shared_ptr<CubeIface> cube_;  // pointer to the cube in polycubed
@@ -61,11 +63,10 @@ class Cube : public BaseCube {
 };
 
 template <class PortType>
-Cube<PortType>::Cube(const std::string &name,
+Cube<PortType>::Cube(const nlohmann::json &conf,
                      const std::vector<std::string> &ingress_code,
-                     const std::vector<std::string> &egress_code,
-                     const CubeType type, LogLevel level)
-    : BaseCube(name, ingress_code, egress_code, type, level) {
+                     const std::vector<std::string> &egress_code)
+    : BaseCube(conf, ingress_code, egress_code) {
   // TODO: move to function
   handle_packet_in = [&](const PacketIn *md,
                          const std::vector<uint8_t> &packet) -> void {
@@ -85,8 +86,8 @@ Cube<PortType>::Cube(const std::string &name,
     packet_in(p, md_, packet);
   };
 
-  cube_ = factory_->create_cube(name, ingress_code, egress_code, handle_log_msg,
-                                type, handle_packet_in, level);
+  cube_ = factory_->create_cube(conf, ingress_code, egress_code, handle_log_msg,
+                                handle_packet_in);
   // TODO: where to keep this reference?, keep a double reference?
   BaseCube::cube_ = cube_;
 }
@@ -109,7 +110,7 @@ std::shared_ptr<PortType> Cube<PortType>::add_port(const std::string &port_name,
     throw std::runtime_error("Port " + port_name + " already exists");
   }
 
-  auto cube_port = cube_->add_port(port_name);
+  auto cube_port = cube_->add_port(port_name, conf.getBase());
 
   try {
     typename std::map<std::string, std::shared_ptr<PortType>>::iterator iter;
@@ -127,7 +128,7 @@ std::shared_ptr<PortType> Cube<PortType>::add_port(const std::string &port_name,
 }
 
 template <class PortType>
-std::vector<std::shared_ptr<PortType>> Cube<PortType>::get_ports() {
+std::vector<std::shared_ptr<PortType>> Cube<PortType>::get_ports() const {
   std::vector<std::shared_ptr<PortType>> ports;
   for (auto &it : ports_by_name_)
     ports.push_back(it.second);
@@ -166,6 +167,16 @@ std::shared_ptr<PortType> Cube<PortType>::get_port(int index) {
                              " does not exist");
   }
   return ports_by_id_.at(index);
+}
+
+template <class PortType>
+void Cube<PortType>::set_conf(const nlohmann::json &conf) {
+  return cube_->set_conf(conf);
+}
+
+template <class PortType>
+nlohmann::json Cube<PortType>::to_json() const {
+  return cube_->to_json();
 }
 
 }  // namespace service

src/libs/polycube/include/polycube/services/cube_factory.h
@@ -21,6 +21,7 @@
 #include <vector>
 
 #include "polycube/common.h"
+#include "polycube/services/json.hpp"
 
 namespace polycube {
 namespace service {
@@ -65,16 +66,14 @@ typedef std::function<void(void)> attach_cb;
 class CubeFactory {
  public:
   virtual std::shared_ptr<CubeIface> create_cube(
-      const std::string &name, const std::vector<std::string> &ingress_code,
+      const nlohmann::json &conf, const std::vector<std::string> &ingress_code,
       const std::vector<std::string> &egress_code, const log_msg_cb &log_msg,
-      const CubeType type, const packet_in_cb &cb = empty_packet_in_cb,
-      LogLevel level = LogLevel::OFF) = 0;
+      const packet_in_cb &cb = empty_packet_in_cb) = 0;
 
   virtual std::shared_ptr<TransparentCubeIface> create_transparent_cube(
-      const std::string &name, const std::vector<std::string> &ingress_code,
+      const nlohmann::json &conf, const std::vector<std::string> &ingress_code,
       const std::vector<std::string> &egress_code, const log_msg_cb &log_msg,
-      const CubeType type, const packet_in_cb &cb, const attach_cb &attach,
-      LogLevel level) = 0;
+      const packet_in_cb &cb, const attach_cb &attach) = 0;
 
   virtual void destroy_cube(const std::string &name) = 0;
 };

src/libs/polycube/include/polycube/services/cube_iface.h
@@ -26,8 +26,6 @@
 
 #include "polycube/services/json.hpp"
 
-using json = nlohmann::json;
-
 namespace polycube {
 namespace service {
 
@@ -71,16 +69,21 @@ class BaseCubeIface {
   virtual const Guid &uuid() const = 0;
   virtual const std::string get_name() const = 0;
 
-  virtual json to_json() const = 0;
+  virtual void set_conf(const nlohmann::json &conf) = 0;
+  virtual nlohmann::json to_json() const = 0;
 };
 
 class CubeIface : virtual public BaseCubeIface {
  public:
-  virtual std::shared_ptr<PortIface> add_port(const std::string &name) = 0;
+  virtual std::shared_ptr<PortIface> add_port(const std::string &name,
+    const nlohmann::json &conf) = 0;
   virtual void remove_port(const std::string &name) = 0;
   virtual std::shared_ptr<PortIface> get_port(const std::string &name) = 0;
 
   virtual void update_forwarding_table(int index, int value) = 0;
+
+  virtual void set_conf(const nlohmann::json &conf) = 0;
+  virtual nlohmann::json to_json() const = 0;
 };
 
 class TransparentCubeIface : virtual public BaseCubeIface {
@@ -91,6 +94,9 @@ class TransparentCubeIface : virtual public BaseCubeIface {
                              const std::string &value) = 0;
   virtual void send_packet_out(const std::vector<uint8_t> &packet, Sense sense,
                                bool recirculate = false) = 0;
+
+  virtual void set_conf(const nlohmann::json &conf) = 0;
+  virtual nlohmann::json to_json() const = 0;
 };
 }
 }

src/libs/polycube/include/polycube/services/port.h
@@ -48,6 +48,9 @@ class Port {
   PortStatus get_status() const;
   PortType get_type() const;
 
+  virtual void set_conf(const nlohmann::json &conf);
+  virtual nlohmann::json to_json() const;
+
  private:
   class impl;
   std::unique_ptr<impl> pimpl_;

commit d55111d871e3721bb17f2a43efbfa78aeb7c8b41

polycubed: show parent when attached to a netdev
A transparent cube can be attached to a standard cube's port or to a netdev,
the conversion to json was failing to show it when the cube was attached to a
netdev.

Fixes: 8884259e7338 ("use json as a configuration mechanishm for cubes and ports")
Reported-by: Matteo Bertrone <m.bertrone@gmail.com>

Signed-off-by: Mauricio Vasquez B <mauriciovasquezbernal@gmail.com>

src/polycubed/src/extiface.cpp
@@ -251,5 +251,9 @@ bool ExtIface::is_used() const {
   return cubes_.size() > 0 || peer_ != nullptr;
 }
 
+std::string ExtIface::get_iface_name() const {
+  return iface_;
+}
+
 }  // namespace polycubed
 }  // namespace polycube

src/polycubed/src/extiface.h
@@ -48,6 +48,8 @@ class ExtIface : public PeerIface {
   void set_next_index(uint16_t index);
   bool is_used() const;
 
+  std::string get_iface_name() const;
+
  protected:
   static std::set<std::string> used_ifaces;
   int load_ingress();

src/polycubed/src/transparent_cube.cpp
@@ -151,8 +151,11 @@ nlohmann::json TransparentCube::to_json() const {
 
   std::string parent;
   if (parent_) {
-    auto port_parent = dynamic_cast<Port *>(parent_);
-    parent = port_parent->get_path();
+    if (auto port_parent = dynamic_cast<Port *>(parent_)) {
+      parent = port_parent->get_path();
+    } else if (auto iface_parent = dynamic_cast<ExtIface *>(parent_)) {
+      parent = iface_parent->get_iface_name();
+    }
   }
 
   j["parent"] = parent;