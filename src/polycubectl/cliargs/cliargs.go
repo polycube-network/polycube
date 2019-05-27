polycube-network/polycube

commit 074ece5b349ace55a23430724d91c65f6ef78152

polycubectl: add reading from standard input capability
This feature allows read the contents of a command from a file, for instance:

polycubectl helloworld add hw0 < hw0_conf.json

Signed-off-by: Riccardo Marchi <riccardo.marchi4@gmail.com>

src/polycubectl/cliargs/cliargs.go
@@ -21,6 +21,11 @@ import (
 	"strconv"
 	"strings"
 
+	"os"
+	"bufio"
+	"bytes"
+	"github.com/ghodss/yaml"
+
 	"github.com/polycube-network/polycube/src/polycubectl/config"
 	"github.com/polycube-network/polycube/src/polycubectl/httprequest"
 
@@ -441,7 +446,25 @@ func (cli *CLIArgs) GetHTTPRequest() (*httprequest.HTTPRequest, error) {
 			url += url0
 			body = body0
 		} else {
-			body = cli.buildBody()
+			// if there is text on the standard input that'll used as the body
+			var buffer bytes.Buffer
+			reader := bufio.NewReader(os.Stdin)
+			text, _ := reader.ReadString('\n')
+			for text != "" {
+				buffer.WriteString(text)
+				text, _ = reader.ReadString('\n')
+			}
+
+			if buffer.Len() > 0 {
+				jsonBuff, err := yaml.YAMLToJSON(buffer.Bytes())
+				if err != nil {
+					return nil, err
+				} else {
+					body = jsonBuff
+				}
+			} else {
+				body = cli.buildBody()
+			}
 		}
 	}
 

src/polycubectl/aliases.go
@@ -18,7 +18,6 @@ package main
 
 import (
 	"fmt"
-	//"reflect"
 	"github.com/polycube-network/polycube/src/polycubectl/cliargs"
 )
 

commit 694e7e2ab4fa037cc1803351f27d4b64b104f79b

polycubectl: fix reading from standard input
After 074ece5b349a ("polycubectl: add reading from standard input capability")
polycubectl tries to read the standard input in all the invocations, this
causes commands typed by the user to hang up.

This commit uses a logic to detect if the standard input is being piped, in
that case it is used.

This bug was probably not detected for two reasons:
- We concentrate in test the new feature, i.e, loading configurations from files,
without testing the standard behaviour.
- The bug is not present in our CI (jenkins) probably because it executes
polycubect without stdin.

Fixes: 074ece5b349a ("polycubectl: add reading from standard input capability")

Reported-by: Gianluca Scopelliti <gianlu.1033@gmail.com>
Signed-off-by: Mauricio Vasquez B <mauriciovasquezbernal@gmail.com>

src/polycubectl/cliargs/cliargs.go
@@ -436,6 +436,11 @@ func (cli *CLIArgs) GetHTTPRequest() (*httprequest.HTTPRequest, error) {
 	var url string
 
 	url = cli.buildURL()
+	var stat os.FileInfo
+	var staterr error
+	if os.Stdin != nil {
+		stat, staterr = os.Stdin.Stat()
+	}
 
 	// TODO: is command == "" required?
 	if !cli.IsHelp && (cli.Command == AddCommand ||
@@ -445,8 +450,10 @@ func (cli *CLIArgs) GetHTTPRequest() (*httprequest.HTTPRequest, error) {
 			url0, body0 := cli.buildSingleParamBody()
 			url += url0
 			body = body0
-		} else {
-			// if there is text on the standard input that'll used as the body
+		} else if os.Stdin != nil && staterr == nil &&
+			stat.Mode() & os.ModeCharDevice == 0 && stat.Size() > 0 {
+			// os.ModeCharDevice flag is set when piping text to the stdin
+			// use text in the standard input as body
 			var buffer bytes.Buffer
 			reader := bufio.NewReader(os.Stdin)
 			text, _ := reader.ReadString('\n')
@@ -465,6 +472,8 @@ func (cli *CLIArgs) GetHTTPRequest() (*httprequest.HTTPRequest, error) {
 			} else {
 				body = cli.buildBody()
 			}
+		} else {
+			body = cli.buildBody()
 		}
 	}
 