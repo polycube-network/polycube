5d3a0fe7c3a1c89f09e0130674a2e69a02af13db
Gianluca Scopelliti <gianlu.1033@gmail.com>
2019-05-28 11:04:23 +0200

    Add implementation of service bridge
    Features:
    - The datapath code is regenerated automatically whenever stp is enabled
    or disabled, so it is not necessary to configure that parameter at the
    creation of the bridge.
    - For each port, there are one or more PortsStp instances according to
    the VLANs allowed for that port.
    - A Stp instance is created automatically, when the first PortStp instance
    for the same VLAN is created.
    - In the same way, a Stp instance is destroyed automatically, when the last
    PortsStp instance for the same VLAN is destroyed.
    - Since the management of Stp/PortsStp instances is done automatically, the
    user is not allowed to create/destroy manually those instances, but only to
    configure their parameters.
    - Containers Access/Trunk do not exist at the same time; if the port is
    configured in access mode the user can't configure trunk parameters and
    vice versa
    
    Signed-off-by: Gianluca Scopelliti <gianlu.1033@gmail.com>

--- src/services/pcn-bridge/src/ext.h
+++ src/services/pcn-bridge/src/ext.h
@@ -14,21 +14,35 @@
  * limitations under the License.
  */
 
-#include "PortsTrunkAllowed.h"
-#include "Bridge.h"
+#pragma once
 
-PortsTrunkAllowed::PortsTrunkAllowed(PortsTrunk &parent,
-                                     const PortsTrunkAllowedJsonObject &conf)
-    : PortsTrunkAllowedBase(parent) {
-  logger()->debug("Creating PortsTrunkAllowed instance");
+#define VLAN_WILDCARD 0x00
 
-  vlan_ = conf.getVlanid();
-}
+enum entry_type { STATIC = 0, DYNAMIC };
 
-PortsTrunkAllowed::~PortsTrunkAllowed() {
-  logger()->debug("Destroying PortsTrunkAllowed instance for vlan {0}", vlan_);
-}
+struct port {
+  uint16_t mode;
+  uint16_t native_vlan;
+  bool native_vlan_enabled;
+} __attribute__((packed));
 
-uint16_t PortsTrunkAllowed::getVlanid() {
-  return vlan_;
-}
+struct port_vlan_key {
+  uint32_t port;
+  uint16_t vlan;
+} __attribute__((packed));
+
+struct port_vlan_value {
+  uint16_t vlan;
+  uint16_t stp_state;
+} __attribute__((packed));
+
+struct fwd_key {
+  uint16_t vlan;
+  uint64_t mac;
+} __attribute__((packed));
+
+struct fwd_entry {
+  uint32_t timestamp;
+  uint32_t port;
+  enum entry_type type;
+} __attribute__((packed));
