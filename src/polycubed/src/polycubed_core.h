polycube-network/polycube

commit fbf9bc95246abadcb62160747368b967a66bbc10

add /cubes/ creation api
This feature allows to load an entire topology in the daemon

by picking the cubes and their set up in a JSON.

Signed-off-by: Riccardo Marchi <riccardo.marchi4@gmail.com>

src/polycubed/src/rest_server.cpp
@@ -171,6 +171,9 @@ void RestServer::setup_routes() {
   router_->get(base + std::string("/cubes/:cubeName"),
                bind(&RestServer::get_cube, this));
 
+  router_->post(base + std::string("/cubes"),
+                bind(&RestServer::post_cubes, this));
+
   router_->options(base + std::string("/cubes"),
                    bind(&RestServer::cubes_help, this));
 
@@ -422,6 +425,31 @@ void RestServer::get_cube(const Pistache::Rest::Request &request,
     auto name = request.param(":cubeName").as<std::string>();
     std::string retJsonStr = core.get_cube(name);
     response.send(Pistache::Http::Code::Ok, retJsonStr);
+  } catch (const std::runtime_error &e) {
+    logger->error("{0}", e.what());
+    response.send(Pistache::Http::Code::Bad_Request, e.what());
+  }
+}
+
+void RestServer::post_cubes(const Pistache::Rest::Request &request,
+                               Pistache::Http::ResponseWriter response) {
+  logRequest(request);
+  try {
+    json j = json::parse(request.body());
+    logJson(j);
+    std::vector<Response> resp = {{ErrorTag::kNoContent, nullptr}};
+    bool error = false;
+    for (auto &it : j) {
+      resp = core.get_service_controller(it["service-name"]).get_management_interface()->get_service()
+              ->CreateReplaceUpdate(it["name"], it, false, true);
+      if (!error && resp[0].error_tag != kCreated) {
+        Rest::Server::ResponseGenerator::Generate(std::move(resp), std::move(response));
+        error = true;
+      }
+    }
+    if (!error) {
+      Rest::Server::ResponseGenerator::Generate(std::move(resp), std::move(response));
+    }
   } catch (const std::runtime_error &e) {
     logger->error("{0}", e.what());
     response.send(Pistache::Http::Code::Bad_Request, e.what());

src/polycubed/src/rest_server.h
@@ -85,6 +85,9 @@ class RestServer {
   void get_cube(const Pistache::Rest::Request &request,
                 Pistache::Http::ResponseWriter response);
 
+    void post_cubes(const Pistache::Rest::Request &request,
+                       Pistache::Http::ResponseWriter response);
+
   void cubes_help(const Pistache::Rest::Request &request,
                   Pistache::Http::ResponseWriter response);
 

src/polycubed/src/server/Resources/Endpoint/Service.cpp
@@ -67,9 +67,8 @@ void Service::ClearCubes() {
   }
 }
 
-void Service::CreateReplaceUpdate(const std::string &name, nlohmann::json &body,
-                                  ResponseWriter response, bool update,
-                                  bool initialization) {
+std::vector<Response>
+Service::CreateReplaceUpdate(const std::string &name, nlohmann::json &body, bool update, bool initialization) {
   if (update || !ServiceController::exists_cube(name)) {
     auto op = OperationType(update, initialization);
     auto k = ListKeyValues{};
@@ -80,9 +79,7 @@ void Service::CreateReplaceUpdate(const std::string &name, nlohmann::json &body,
 
     auto body_errors = BodyValidate(name, k, jbody, initialization);
     if (!body_errors.empty()) {
-      Server::ResponseGenerator::Generate(std::move(body_errors),
-                                          std::move(response));
-      return;
+      return std::move(body_errors);
     }
 
     auto resp = WriteValue(name, body, k, op);
@@ -91,12 +88,9 @@ void Service::CreateReplaceUpdate(const std::string &name, nlohmann::json &body,
                     resp.error_tag == ErrorTag::kNoContent)) {
       cube_names_.AddValue(name);
     }
-    Server::ResponseGenerator::Generate(std::vector<Response>{resp},
-                                        std::move(response));
+    return std::vector<Response>{resp};
   } else {
-    Server::ResponseGenerator::Generate(
-        std::vector<Response>{{ErrorTag::kDataExists, nullptr}},
-        std::move(response));
+    return std::vector<Response>{{ErrorTag::kDataExists, nullptr}};
   }
 }
 
@@ -127,8 +121,8 @@ void Service::post_body(const Request &request, ResponseWriter response) {
     return;
   }
 
-  CreateReplaceUpdate(body["name"].get<std::string>(), body,
-                      std::move(response), false, true);
+  auto resp = CreateReplaceUpdate(body["name"].get<std::string>(), body, false, true);
+  Server:: ResponseGenerator::Generate(std::move(resp), std::move(response));
 }
 
 void Service::post(const Request &request, ResponseWriter response) {
@@ -149,7 +143,8 @@ void Service::post(const Request &request, ResponseWriter response) {
     }
   }
 
-  CreateReplaceUpdate(name, body, std::move(response), false, true);
+  auto resp = CreateReplaceUpdate(name, body, false, true);
+  Server:: ResponseGenerator::Generate(std::move(resp), std::move(response));
 }
 
 void Service::put(const Request &request, ResponseWriter response) {
@@ -161,7 +156,8 @@ void Service::put(const Request &request, ResponseWriter response) {
     body = nlohmann::json::parse(request.body());
   }
   body["name"] = name;
-  CreateReplaceUpdate(name, body, std::move(response), false, true);
+  auto resp = CreateReplaceUpdate(name, body, false, true);
+  Server:: ResponseGenerator::Generate(std::move(resp), std::move(response));
 }
 
 void Service::patch(const Request &request, ResponseWriter response) {
@@ -173,7 +169,8 @@ void Service::patch(const Request &request, ResponseWriter response) {
     body = nlohmann::json::parse(request.body());
   }
   body["name"] = name;
-  CreateReplaceUpdate(name, body, std::move(response), true, false);
+  auto resp = CreateReplaceUpdate(name, body, true, false);
+  Server:: ResponseGenerator::Generate(std::move(resp), std::move(response));
 }
 
 void Service::del(const Pistache::Rest::Request &request,
@@ -201,8 +198,8 @@ void Service::patch_body(const Request &request, ResponseWriter response) {
         std::move(response));
     return;
   }
-  CreateReplaceUpdate(body["name"].get<std::string>(), body,
-                      std::move(response), true, false);
+  auto resp = CreateReplaceUpdate(body["name"].get<std::string>(), body, true, false);
+  Server:: ResponseGenerator::Generate(std::move(resp), std::move(response));
 }
 
 void Service::options_body(const Request &request, ResponseWriter response) {

src/polycubed/src/server/Resources/Endpoint/Service.h
@@ -56,13 +56,12 @@ class Service : public ParentResource, public Body::Service {
 
   virtual Response ReadHelp() = 0;
 
+  std::vector<Response> CreateReplaceUpdate(const std::string &name, nlohmann::json &body,
+                                            bool update, bool initialization);
+
  private:
   const std::string body_rest_endpoint_;
   Validators::InSetValidator cube_names_;
-
-  void CreateReplaceUpdate(const std::string &name, nlohmann::json &body,
-                           ResponseWriter response, bool replace,
-                           bool initialization);
 
   nlohmann::json getServiceKeys() const;
 

commit bbfd431b6c83342bffe7ff646b148aa5dd622f6e

polycubed: set peers after all cubes are created
When a list of cubes is created this is possible that a port in a cube has as
peer another port in a cube that hasn't been created yet, this causes an
exception that aborts the port's creation.

This commit strips the port's peer and sets them after all cubes are created.

Fixes: fbf9bc95246a ("add /cubes/ creation api")
Signed-off-by: Mauricio Vasquez B <mauriciovasquezbernal@gmail.com>

src/polycubed/src/polycubed_core.h
@@ -39,6 +39,7 @@ namespace polycubed {
 class RestServer;
 
 class PolycubedCore {
+  friend class RestServer;
  public:
   PolycubedCore(BaseModel *base_model);
   ~PolycubedCore();

src/polycubed/src/rest_server.cpp
@@ -436,20 +436,33 @@ void RestServer::post_cubes(const Pistache::Rest::Request &request,
                                Pistache::Http::ResponseWriter response) {
   logRequest(request);
   try {
-    json j = json::parse(request.body());
-    logJson(j);
+    json cubes = json::parse(request.body());
+    logJson(cubes);
+    /*
+     * This is possible that a cube's port has as peer another port
+     * of a cube that hasn't been created yet.  This logic removes all
+     * the peers from the request and sets them after all cubes have
+     * been created.
+     */
+    auto peers = utils::strip_port_peers(cubes);
     std::vector<Response> resp = {{ErrorTag::kNoContent, nullptr}};
     bool error = false;
-    for (auto &it : j) {
-      resp = core.get_service_controller(it["service-name"]).get_management_interface()->get_service()
-              ->CreateReplaceUpdate(it["name"], it, false, true);
+
+    for (auto &cube : cubes) {
+      resp = core.get_service_controller(cube["service-name"]).get_management_interface()->get_service()
+              ->CreateReplaceUpdate(cube["name"], cube, false, true);
       if (!error && resp[0].error_tag != kCreated) {
         Rest::Server::ResponseGenerator::Generate(std::move(resp), std::move(response));
         error = true;
       }
     }
+
     if (!error) {
       Rest::Server::ResponseGenerator::Generate(std::move(resp), std::move(response));
+    }
+
+    for (auto &[peer1, peer2] : peers) {
+      core.try_to_set_peer(peer1, peer2);
     }
   } catch (const std::runtime_error &e) {
     logger->error("{0}", e.what());

src/polycubed/src/utils/utils.cpp
@@ -44,6 +44,29 @@ bool check_kernel_version(const std::string &version) {
          KERNEL_VERSION(major_r, minor_r, patch_r);
 }
 
+std::map<std::string, std::string> strip_port_peers(json &cubes) {
+  std::map<std::string, std::string> peers;
+
+  for (auto &cube : cubes) {
+    if (!cube.count("ports")) {
+      continue;
+    }
+
+    auto &ports = cube.at("ports");
+
+    for (auto &port : ports) {
+      if (port.count("peer")) {
+        auto cube_name = cube.at("name").get<std::string>();
+        auto port_name = port.at("name").get<std::string>();
+        peers[cube_name + ":" + port_name] = port.at("peer").get<std::string>();
+        port.erase("peer");
+      }
+    }
+  }
+
+  return peers;
+}
+
 }  // namespace utils
 }  // namespace polycubed
 }  // namespace polycube

src/polycubed/src/utils/utils.h
@@ -15,12 +15,17 @@
  */
 
 #include <string>
+#include <map>
+#include "polycube/services/json.hpp"
+
+using json = nlohmann::json;
 
 namespace polycube {
 namespace polycubed {
 namespace utils {
 
 bool check_kernel_version(const std::string &version);
+std::map<std::string, std::string> strip_port_peers(json &cubes);
 
 }  // namespace utils
 }  // namespace polycubed