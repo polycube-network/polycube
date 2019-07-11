d6097da96b88a74a1079acc1d89c73dff623dac5
Mauricio Vasquez B <mauriciovasquezbernal@gmail.com>
2019-07-11 18:21:38 -0500

    cleanup install:
    - remove commented lines about golang
    - use DEBIAN_FRONTEND=noninteractive to avoid remaining blocked
    
    Signed-off-by: Mauricio Vasquez B <mauriciovasquezbernal@gmail.com>

--- scripts/install.sh
+++ scripts/install.sh
@@ -61,22 +61,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p $WORKDIR
 
 $SUDO apt update
-$SUDO apt install -y wget gnupg2
+$SUDO apt install -y wget gnupg2 software-properties-common
 
-echo "Install golang v1.12"
-# TODO: v1.12 still not available in repos
-#wget https://dl.google.com/go/go1.12.6.linux-amd64.tar.gz
-#$SUDO tar -xvf go1.12.6.linux-amd64.tar.gz
-#$SUDO mv go /usr/local
-#$SUDO ln -s /usr/local/go/bin/go /usr/bin
+# golang v1.12 still not available in repos
+$SUDO add-apt-repository ppa:longsleep/golang-backports -y
 
-$SUDO apt-get install -y software-properties-common
-$SUDO add-apt-repository ppa:longsleep/golang-backports
-$SUDO apt-get update
-$SUDO apt install -y golang-go
-#$SUDO ln -s /usr/bin
-
-
+# repo for libyang-dev
 $SUDO sh -c "echo 'deb http://download.opensuse.org/repositories/home:/liberouter/xUbuntu_18.04/ /' > /etc/apt/sources.list.d/home:liberouter.list"
 wget -nv https://download.opensuse.org/repositories/home:liberouter/xUbuntu_18.04/Release.key -O Release.key
 $SUDO apt-key add - < Release.key
@@ -89,8 +79,7 @@ PACKAGES+=" bison flex libelf-dev" # bcc dependencies
 PACKAGES+=" libllvm5.0 llvm-5.0-dev libclang-5.0-dev" # bpf tools compilation tool chain
 PACKAGES+=" libnl-route-3-dev libnl-genl-3-dev" # netlink library
 PACKAGES+=" uuid-dev"
-#PACKAGES+=" golang-go" # needed for polycubectl and pcn-k8s
-#PACKAGES+=" golang-1.12-go" # needed for polycubectl and pcn-k8s
+PACKAGES+=" golang-go" # needed for polycubectl and pcn-k8s
 PACKAGES+=" pkg-config"
 PACKAGES+=" libyang-dev"
 PACKAGES+=" autoconf libtool m4 automake"
@@ -105,7 +94,8 @@ if [ "$MODE" == "pcn-k8s" ]; then
   PACKAGES+=" iproute2" # provides bridge command that is used to add entries in vxlan device
 fi
 
-$SUDO  apt install -y $PACKAGES
+# use non inreractive to avoid blocking the install script
+$SUDO bash -c "DEBIAN_FRONTEND=noninteractive apt-get install -yq $PACKAGES"
 
 echo "Install pistache"
 cd $WORKDIR
