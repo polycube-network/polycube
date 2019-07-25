f1d3036323784c4d394bdb106cd82f0fbf4d66ed
Elis Lulja <elis.lulja@gmail.com>
2019-07-25 10:11:56 +0200

    pcn-k8s: Enforce a policy
    Rules are stored in memory and then are injected in all linked pods' firewalls.
    
    Signed-off-by: Elis Lulja <elis.lulja@gmail.com>

--- src/components/k8s/pcn_k8s/networkpolicies/pcn_firewall/pcn_firewall.go
+++ src/components/k8s/pcn_k8s/networkpolicies/pcn_firewall/pcn_firewall.go
@@ -2,6 +2,7 @@ package pcnfirewall
 
 import (
 	"net"
+	"strings"
 	"sync"
 
 	pcn_controllers "github.com/polycube-network/polycube/src/components/k8s/pcn_k8s/controllers"
@@ -30,6 +31,8 @@ type PcnFirewall interface {
 	// Selector returns the namespace and labels of the pods
 	// monitored by this firewall manager
 	Selector() (map[string]string, string)
+	// EnforcePolicy enforces the provided policy.
+	EnforcePolicy(policyName, policyType string, ingress, egress []k8sfirewall.ChainRule)
 }
 
 // FirewallManager is the implementation of the firewall manager.
@@ -162,6 +165,158 @@ func (d *FirewallManager) Name() string {
 	return d.name
 }
 
+// EnforcePolicy enforces the provided policy.
+func (d *FirewallManager) EnforcePolicy(policyName, policyType string, ingress, egress []k8sfirewall.ChainRule) {
+	// Only one policy at a time, please
+	d.lock.Lock()
+	defer d.lock.Unlock()
+
+	l := log.New().WithFields(log.Fields{"by": "FirewallManager-" + d.name, "method": "EnforcePolicy"})
+	l.Infof("Enforcing policy %s...", policyName)
+
+	//-------------------------------------
+	// Store the rules
+	//-------------------------------------
+
+	ingressIDs, egressIDs := d.storeRules(policyName, "", ingress, egress)
+
+	//-------------------------------------
+	// Inject the rules on each firewall
+	//-------------------------------------
+
+	if len(d.linkedPods) == 0 {
+		l.Infoln("There are no linked pods. Stopping here.")
+		return
+	}
+
+	var injectWaiter sync.WaitGroup
+	injectWaiter.Add(len(d.linkedPods))
+
+	for _, ip := range d.linkedPods {
+		name := "fw-" + ip
+		go d.injecter(name, ingressIDs, egressIDs, &injectWaiter, 0, 0)
+	}
+	injectWaiter.Wait()
+}
+
+// storeRules stores rules in memory according to their policy
+func (d *FirewallManager) storeRules(policyName, target string, ingress, egress []k8sfirewall.ChainRule) ([]k8sfirewall.ChainRule, []k8sfirewall.ChainRule) {
+	if _, exists := d.ingressRules[policyName]; !exists {
+		d.ingressRules[policyName] = []k8sfirewall.ChainRule{}
+	}
+	if _, exists := d.egressRules[policyName]; !exists {
+		d.egressRules[policyName] = []k8sfirewall.ChainRule{}
+	}
+
+	description := "policy=" + policyName
+	newIngress := make([]k8sfirewall.ChainRule, len(ingress))
+	newEgress := make([]k8sfirewall.ChainRule, len(egress))
+
+	// --- ingress
+	for i, rule := range ingress {
+		newIngress[i] = rule
+		newIngress[i].Description = description
+		if len(target) > 0 {
+			newIngress[i].Dst = target
+		}
+
+		d.ingressRules[policyName] = append(d.ingressRules[policyName], newIngress[i])
+	}
+
+	// -- Egress
+	for i, rule := range egress {
+		newEgress[i] = rule
+		newEgress[i].Description = description
+		if len(target) > 0 {
+			newEgress[i].Src = target
+		}
+
+		d.egressRules[policyName] = append(d.egressRules[policyName], newEgress[i])
+	}
+	return newIngress, newEgress
+}
+
+// injecter is a convenient method for injecting rules for a single firewall
+// for both directions
+func (d *FirewallManager) injecter(firewall string, ingressRules, egressRules []k8sfirewall.ChainRule, waiter *sync.WaitGroup, iStartFrom, eStartFrom int32) error {
+	l := log.New().WithFields(log.Fields{"by": "FirewallManager-" + d.name, "method": "Injecter(" + firewall + ", ...)"})
+
+	// Should I notify caller when I'm done?
+	if waiter != nil {
+		defer waiter.Done()
+	}
+
+	// Is firewall ok?
+	if ok, err := d.isFirewallOk(firewall); !ok {
+		l.Errorf("Could not inject rules. Firewall is not ok: %s", err)
+		return err
+	}
+
+	//-------------------------------------
+	// Inject rules direction concurrently
+	//-------------------------------------
+	var injectWaiter sync.WaitGroup
+	injectWaiter.Add(2)
+	defer injectWaiter.Wait()
+
+	go d.injectRules(firewall, "ingress", ingressRules, &injectWaiter, iStartFrom)
+	go d.injectRules(firewall, "egress", egressRules, &injectWaiter, eStartFrom)
+
+	return nil
+}
+
+// injectRules is a wrapper for firewall's CreateFirewallChainRuleListByID
+// and CreateFirewallChainApplyRulesByID methods.
+func (d *FirewallManager) injectRules(firewall, direction string, rules []k8sfirewall.ChainRule, waiter *sync.WaitGroup, startFrom int32) error {
+	l := log.New().WithFields(log.Fields{"by": "FirewallManager-" + d.name, "method": "injectRules(" + firewall + "," + direction + ",...)"})
+
+	// Should I notify caller when I'm done?
+	if waiter != nil {
+		defer waiter.Done()
+	}
+
+	//-------------------------------------
+	// Inject & apply
+	//-------------------------------------
+	// The ip of the pod we are protecting. Used for the SRC or the DST
+	me := strings.Split(firewall, "-")[1]
+
+	// We are using the insert call here, which adds the rule on the startFrom id
+	// and pushes the other rules downwards. In order to preserve original order,
+	// we're going to start injecting from the last to the first.
+
+	len := len(rules)
+	for i := len - 1; i > -1; i-- {
+		ruleToInsert := k8sfirewall.ChainInsertInput(rules[i])
+		ruleToInsert.Id = startFrom
+
+		// There is only the pod on the other side of the link, so all packets
+		// travelling there are obviously either going to or from the pod.
+		// So there is no need to include the pod as a destination or source
+		// in the rules. But it helps to keep them clearer and precise.
+		if direction == "ingress" {
+			ruleToInsert.Src = me
+		} else {
+			ruleToInsert.Dst = me
+		}
+
+		_, response, err := d.fwAPI.CreateFirewallChainInsertByID(nil, firewall, direction, ruleToInsert)
+		if err != nil {
+			l.Errorf("Error while trying to inject rule: %s, %+v", err, response)
+			// This rule had an error, but we still gotta push the other ones dude...
+			//return err
+		}
+	}
+
+	// Now apply the changes
+	if response, err := d.applyRules(firewall, direction); err != nil {
+		l.Errorf("Error while trying to apply rules: %s, %+v", err, response)
+		return err
+	}
+
+	return nil
+}
+
 // Selector returns the namespace and labels of the pods
 // monitored by this firewall manager
 func (d *FirewallManager) Selector() (map[string]string, string) {
@@ -176,4 +331,19 @@ func (d *FirewallManager) isFirewallOk(firewall string) (bool, error) {
 		return false, err
 	}
 	return true, nil
+}
+
+// updateDefaultAction is a wrapper for UpdateFirewallChainDefaultByID method.
+func (d *FirewallManager) updateDefaultAction(firewall, direction, to string) error {
+	// To is enclosed between \" because otherwise the swagger-generated API
+	// would wrongly send forward instead of "forward".
+	actualTo := "\"" + to + "\""
+	_, err := d.fwAPI.UpdateFirewallChainDefaultByID(nil, firewall, direction, actualTo)
+	return err
+}
+
+// applyRules is a wrapper for CreateFirewallChainApplyRulesByID method.
+func (d *FirewallManager) applyRules(firewall, direction string) (bool, error) {
+	out, _, err := d.fwAPI.CreateFirewallChainApplyRulesByID(nil, firewall, direction)
+	return out.Result, err
 }
