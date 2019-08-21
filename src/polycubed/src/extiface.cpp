8ac349c38bbb4d5488731388c71dcfddf55e8957
francescomessina <francescomessina92@hotmail.com>
2019-08-21 14:52:20 +0200

    libpolycube: add a new parameter to the get_parameter in the extiface
    Signed-off-by: Francesco Messina <francescomessina92@hotmail.com>

--- src/polycubed/src/extiface.cpp
+++ src/polycubed/src/extiface.cpp
@@ -30,6 +30,7 @@ namespace polycubed {
 
 const std::string PARAMETER_MAC = "MAC";
 const std::string PARAMETER_IP = "IP";
+const std::string PARAMETER_PEER = "PEER";
 
 std::set<std::string> ExtIface::used_ifaces;
 
@@ -372,6 +373,8 @@ std::string ExtIface::get_parameter(const std::string &param_name) {
       // netdev does not have an Ip
       return "";
     }
+  } else if (param_upper == PARAMETER_PEER) {
+    return iface_;
   } else {
     throw std::runtime_error("parameter " + param_upper +
                              " not available in extiface");
