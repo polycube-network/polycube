d5bb3d7977d929a0edb956ab64e7b42a9330659d
Alex Palesandro <alex.palesandro@carrefour.com>
2019-12-11 11:16:09 +0100

    Fixing Libyang library version

--- scripts/pre-requirements.sh
+++ scripts/pre-requirements.sh
@@ -11,24 +11,7 @@ $SUDO bash -c "apt install --allow-unauthenticated -y wget gnupg2 software-prope
 # golang v1.12 still not available in repos
 $SUDO add-apt-repository ppa:longsleep/golang-backports -y || true
 
-# repo for libyang-dev
-
-# Cannot add the repository to our machine, as the GPG key expired on Aug 2019 and Ubuntu refuses to install the software in there
-#$SUDO sh -c "echo 'deb http://download.opensuse.org/repositories/home:/liberouter/xUbuntu_18.04/ /' > /etc/apt/sources.list.d/home:liberouter.list"
-#wget -nv https://download.opensuse.org/repositories/home:liberouter/xUbuntu_18.04/Release.key -O Release.key
-#$SUDO apt-key add - < Release.key
-# So, installing the required package by downloading it manually
-apt remove -y libyang libyang-dev
-wget -nv http://download.opensuse.org/repositories/home:/liberouter/xUbuntu_18.04/amd64/libyang_1.0.101_amd64.deb -O libyang.deb
-wget -nv http://download.opensuse.org/repositories/home:/liberouter/xUbuntu_18.04/amd64/libyang-dev_1.0.101_amd64.deb -O libyang-dev.deb
-$SUDO apt install -f ./libyang.deb
-$SUDO apt install -y -f ./libyang-dev.deb
-rm ./libyang.deb
-rm ./libyang-dev.deb
-
 $SUDO apt update
-
-
 PACKAGES=""
 PACKAGES+=" git" # needed to clone dependencies
 PACKAGES+=" build-essential cmake" # provides compiler and other compilation tools
@@ -51,6 +34,18 @@ if [ "$MODE" == "pcn-k8s" ]; then
   PACKAGES+=" iptables" # only for pcn-k8s
   PACKAGES+=" iproute2" # provides bridge command that is used to add entries in vxlan device
 fi
+
+# licd $WORKDIR
+set +e
+if [ ! -d libyang ]; then
+    git clone https://github.com/CESNET/libyang.git
+fi
+cd libyang
+git checkout v0.14-r1
+mkdir -p build && cd build
+cmake ..
+make -j $(getconf _NPROCESSORS_ONLN)
+$SUDO make install
 
 # use non interactive to avoid blocking the install script
 $SUDO bash -c "DEBIAN_FRONTEND=noninteractive apt-get install -yq $PACKAGES"
