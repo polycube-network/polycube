d7ba1e3dda64ceccc7092404556a065258c9dcce
pinoOgni <ognibenegiuseppe8@gmail.com>
2020-12-27 18:43:46 +0100

    new version test_prometheus_metrics to overcome the problem of order of results returned by curl

--- tests/test_prometheus_metrics.sh
+++ tests/test_prometheus_metrics.sh
@@ -3,46 +3,18 @@
 source "${BASH_SOURCE%/*}/helpers.bash"
 
 # the expected result
-result='# HELP ddos_stats_pkts_packets Total Dropped Packets
-# TYPE ddos_stats_pkts_packets counter
-ddos_stats_pkts_packets{cubeName="d1"} 0.000000
-# HELP k8switch_service_client_exported_services Number of services exported to the client
-# TYPE k8switch_service_client_exported_services gauge
-# HELP k8switch_backend_servers Number of backend servers that actually serve requests
-# TYPE k8switch_backend_servers gauge
-# HELP k8switch_fwd_table_entries Number of entries associated with the forwarding table
-# TYPE k8switch_fwd_table_entries gauge
-# HELP router_ip_ports Number of ports of the router with ip
-# TYPE router_ip_ports gauge
-router_ip_ports{cubeName="r1"} 2.000000
-# HELP router_secondaryip_ports Number of secondary ip addresses of the router
-# TYPE router_secondaryip_ports gauge
-router_secondaryip_ports{cubeName="r1"} 3.000000
-# HELP router_ports Number of ports on the router
-# TYPE router_ports gauge
-router_ports{cubeName="r1"} 2.000000
-# HELP router_route_routes Number of entries in routing table
-# TYPE router_route_routes gauge
-router_route_routes{cubeName="r1"} 2.000000
-# HELP router_arp_table_entries Number of entries in arp table
-# TYPE router_arp_table_entries gauge
-router_arp_table_entries{cubeName="r1"} 0.000000
-# HELP simplebridge_entry_fdb_number_entries Number of entries associated with the filtering database
-# TYPE simplebridge_entry_fdb_number_entries gauge
-# HELP ddos_blacklist_src_addresses Number of addresses in blacklist-src
-# TYPE ddos_blacklist_src_addresses gauge
-ddos_blacklist_src_addresses{cubeName="d1"} 4.000000
-# HELP ddos_blacklist_dst_addresses Number of addresses in blacklist-dst
-# TYPE ddos_blacklist_dst_addresses gauge
-ddos_blacklist_dst_addresses{cubeName="d1"} 0.000000
-# HELP simpleforwarder_actions_entries Number of entries of the Actions table
-# TYPE simpleforwarder_actions_entries gauge
-# HELP pbforwarder_rules_number_rules Number of rules
-# TYPE pbforwarder_rules_number_rules gauge'
+expected_results=('ddos_stats_pkts_packets{cubeName="d1"} 0.000000'
+'ddos_stats_pkts_packets{cubeName="d1"} 0.000000'
+'ddos_blacklist_src_addresses{cubeName="d1"} 4.000000'
+'ddos_blacklist_dst_addresses{cubeName="d1"} 0.000000'
+'router_ip_ports{cubeName="r1"} 2.000000'
+'router_secondaryip_ports{cubeName="r1"} 3.000000'
+'router_ports{cubeName="r1"} 2.000000'
+'router_route_routes{cubeName="r1"} 2.000000'
+'router_arp_table_entries{cubeName="r1"} 0.000000')
 
 set -e
 set -x
-
 
 function cleanup {
   set +e
@@ -79,11 +51,23 @@ polycubectl r1 ports p1 secondaryip add 10.1.1.11/24
 
 polycubectl r1 ports p1 secondaryip add 10.1.1.12/24
 
+curl_result=$(curl -s localhost:9000/polycube/v1/metrics)
 
-diff -q <(echo "$result") <(echo "$(curl -s 'localhost:9000/polycube/v1/metrics')")
+ok=0
+
+for i in "${expected_results[@]}"
+do
+  if [[ "$curl_result" == *"$i"* ]]
+  then
+    ((ok=ok+1))
+  fi
+done
+
+diff -q <(echo "$ok") <(echo "${#expected_results[@]}")
 
 polycubectl del d1
 polycubectl del r1
+
 
 set +x
 trap - EXIT
