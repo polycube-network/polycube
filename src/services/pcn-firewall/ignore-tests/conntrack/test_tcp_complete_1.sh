d8620e07fb5b020038d281f4d512374ff67c9118
Fulvio Risso <fulvio.risso@polito.it>
2021-04-06 08:26:56 +0200

    Updated with latest's Simone patches
    Need to be merged after Simone's PR on Nov 2020.

--- src/services/pcn-firewall/ignore-tests/conntrack/test_tcp_complete_1.sh
+++ src/services/pcn-firewall/ignore-tests/conntrack/test_tcp_complete_1.sh
@@ -2,6 +2,13 @@
 
 source "${BASH_SOURCE%/*}/../helpers.bash"
 
+function fwsetup {
+  polycubectl firewall add fw
+  polycubectl attach fw veth1
+  polycubectl firewall fw chain INGRESS set default=DROP
+  polycubectl firewall fw chain EGRESS set default=DROP
+}
+
 function fwcleanup {
   set +e
   polycubectl firewall del fw
@@ -12,26 +19,25 @@ function fwcleanup {
 trap fwcleanup EXIT
 set -e
 
-echo "TCP Conntrack Test (ns2->ns1) [No automatic forward][Interactive mode]"
+echo "TCP Conntrack Test (ns2->ns1) [No automatic ACCEPT][Interactive mode]"
 
 create_veth 2
 
-polycubectl firewall add fw loglevel=DEBUG
-polycubectl attach fw veth1
+fwsetup
 
 # Allowing connections to be started only from NS2 to NS1
-polycubectl firewall fw chain INGRESS append l4proto=TCP conntrack=ESTABLISHED action=FORWARD > /dev/null
+polycubectl firewall fw chain INGRESS append l4proto=TCP conntrack=ESTABLISHED action=ACCEPT > /dev/null
 polycubectl firewall fw chain INGRESS append conntrack=INVALID action=DROP > /dev/null
 
-polycubectl firewall fw chain EGRESS append l4proto=TCP conntrack=NEW action=FORWARD > /dev/null
-polycubectl firewall fw chain EGRESS append l4proto=TCP conntrack=ESTABLISHED action=FORWARD > /dev/null
+polycubectl firewall fw chain EGRESS append l4proto=TCP conntrack=NEW action=ACCEPT > /dev/null
+polycubectl firewall fw chain EGRESS append l4proto=TCP conntrack=ESTABLISHED action=ACCEPT > /dev/null
 polycubectl firewall fw chain EGRESS append conntrack=INVALID action=DROP > /dev/null
 
 #listen and connect
 set -e
 
 echo "Starting netcat server"
-sudo ip netns exec ns1 netcat -l 60123&
+sudo ip netns exec ns1 netcat -l 60123 &
 sleep 3
 
 echo "Starting netcat client"
